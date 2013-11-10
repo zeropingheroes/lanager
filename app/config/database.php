66fb293f5ce3774a3f0f6dadcda2314c0ae13f83
ilumos <ilumos@gmail.com>
2013-11-10 17:48:27 +0000

    Adding default database connection details

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'lanager',
+			'username'  => 'lanager',
+			'password'  => 'lanager',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
