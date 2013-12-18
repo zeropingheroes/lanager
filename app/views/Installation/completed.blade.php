c14f23721ab402460778e45e0e7b3c7112a2f400
ilumos <ilumos@gmail.com>
2013-12-18 18:14:14 +0000

    Added instructions to change session driver

--- app/views/Installation/completed.blade.php
+++ app/views/Installation/completed.blade.php
@@ -3,10 +3,18 @@
 
 <h2>Installation Completed</h2>
 
-<p>To start using the LANager, in the file <em>/app/config/packages/zeropingheroes/lanager-core/config.php</em> set the following:</p>
+<p><strong>Before using the LANager</strong>, please edit the following config files:</p>
+
+<p><em>/app/config/packages/zeropingheroes/lanager-core/config.php</em></p>
 
 <pre>
 'installationCompleted'	=> true,
 </pre>
 
-<p>Once complete, you may {{ link_to('/', 'continue to the LANager') }}</p>
+<p><em>/app/config/session.php</em></p>
+
+<pre>
+'driver'	=> 'database',
+</pre>
+
+<p>Once you have made these changes, you may {{ link_to('/', 'continue to the LANager!') }}</p>
