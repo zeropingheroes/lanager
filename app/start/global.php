71f401a34f2713be0965d319bf12b8b4bafacf3d
ilumos <ilumos@gmail.com>
2014-03-22 03:39:20 +0000

    Added additional folders to classload

--- app/start/global.php
+++ app/start/global.php
@@ -16,7 +16,13 @@ ClassLoader::addDirectories(array(
 	app_path().'/commands',
 	app_path().'/controllers',
 	app_path().'/models',
-	app_path().'/database/seeds',
+	app_path().'/entities',
+	app_path().'/helpers',
+	app_path().'/interfaces',
+	app_path().'/migrations',
+	app_path().'/repositories',
+	app_path().'/seeds',
+	app_path().'/validators',
 
 ));
 
