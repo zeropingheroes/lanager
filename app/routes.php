cd9b9d2307f90cb38df064eada16ac30b707198c
ilumos <ilumos@gmail.com>
2014-03-22 02:41:23 +0000

    Migrated routes

--- app/routes.php
+++ app/routes.php
@@ -2,11 +2,140 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Route Patterns
 |--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
 */
+
+Route::pattern('user', '[0-9]+');
+Route::pattern('shout', '[0-9]+');
+Route::pattern('event', '[0-9]+');
+Route::pattern('playlist', '[0-9]+');
+
+/*
+|--------------------------------------------------------------------------
+| Users
+|--------------------------------------------------------------------------
+*/
+Route::get(
+	'users/openidlogin',
+	array(
+		'as' => 'users.openIdLogin',
+		'uses' => 'Zeropingheroes\Lanager\UsersController@openIdLogin')
+);
+
+Route::get(
+	'users/logout',
+	array(
+		'as' => 'users.logout',
+		'uses' => 'Zeropingheroes\Lanager\UsersController@logout')
+);
+
+Route::group(array('before' => 'hasRole:SuperAdmin'), function()
+{
+	Route::get(
+		'users/{user}/roles',
+		array(
+			'as' => 'users.roles.edit',
+			'uses' => 'Zeropingheroes\Lanager\UsersController@editRoles')
+	);
+	Route::put(
+		'users/{user}/roles',
+		array(
+			'as' => 'users.roles.update',
+			'uses' => 'Zeropingheroes\Lanager\UsersController@updateRoles')
+	);
+});
+Route::resource('user', 'Zeropingheroes\Lanager\UsersController');
+
+/*
+|--------------------------------------------------------------------------
+| Info Pages
+|--------------------------------------------------------------------------
+*/
+Route::resource('infoPage', 'Zeropingheroes\Lanager\InfoPageController');
+
+/*
+|--------------------------------------------------------------------------
+| Shouts
+|--------------------------------------------------------------------------
+*/
+Route::get(
+	'shouts/{shout}/pin',
+	array(
+		'as' => 'shouts.pin',
+		'uses' => 'Zeropingheroes\Lanager\ShoutsController@pin')
+);
+Route::resource('shouts', 'Zeropingheroes\Lanager\ShoutsController');
+
+/*
+|--------------------------------------------------------------------------
+| Statistics
+|--------------------------------------------------------------------------
+*/
+Route::get(
+	'statistics/applications/current-usage',
+	array(
+		'as' => 'statistics.applications.current-usage',
+		'uses' => 'Zeropingheroes\Lanager\StatesController@currentApplicationUsage')
+);
+Route::get(
+	'statistics/servers/current-usage',
+	array(
+		'as' => 'statistics.servers.current-usage',
+		'uses' => 'Zeropingheroes\Lanager\StatesController@currentServerUsage')
+);
+
+/*
+|--------------------------------------------------------------------------
+| Events
+|--------------------------------------------------------------------------
+*/
+Route::get('events/timetable', 
+	array(
+		'as' => 'events.timetable',
+		'uses' => 'Zeropingheroes\Lanager\EventsController@timetable')
+);
+Route::get(
+	'events/{event}/join',
+	array(
+		'as' => 'events.join',
+		'uses' => 'Zeropingheroes\Lanager\EventsController@join')
+);
+Route::get(
+	'events/{event}/leave',
+	array(
+		'as' => 'events.leave',
+		'uses' => 'Zeropingheroes\Lanager\EventsController@leave')
+);
+Route::resource('events', 'Zeropingheroes\Lanager\EventsController');
+
+/*
+|--------------------------------------------------------------------------
+| Events
+|--------------------------------------------------------------------------
+*/
+Route::get(
+	'playlists/{playlist}/items/current',
+	array(
+		'as' => 'playlists.items.current',
+		'uses' => 'Zeropingheroes\Lanager\PlaylistItemsController@current')
+);
+Route::get(
+	'playlists/{playlist}/items/{playlistItem}/pause',
+	array(
+		'as' => 'playlists.items.pause',
+		'uses' => 'Zeropingheroes\Lanager\PlaylistItemsController@pause')
+);
+Route::resource('playlists', 'Zeropingheroes\Lanager\PlaylistsController');
+Route::resource('playlists.items', 'Zeropingheroes\Lanager\PlaylistItemsController');
+
+
+/*
+|--------------------------------------------------------------------------
+| Index
+|--------------------------------------------------------------------------
+*/
+Route::get('/', function()
+{
+	return Redirect::to('shouts');
+});
