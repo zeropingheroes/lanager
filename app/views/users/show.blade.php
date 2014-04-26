4fb16890d5b3adcd83871581636298dd6430b1a6
ilumos <ilumos@gmail.com>
2014-04-26 13:45:14 +0100

    Warned users with private steam profiles

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -2,6 +2,10 @@
 @section('content')
 
 <?php $state = $user->states()->latest()->first(); ?>
+@if($user->steam_visibility == 1 && Auth::check() && $user->id == Auth::user()->id)
+	{{ Alert::error('<strong>Oh no!</strong> Your Steam Profile is set to private. This means the LANager can\'t add you to the "popular games" or "popular servers" pages - please consider ' .
+	link_to(SteamBrowserProtocol::openSteamPage('SteamIDEditPage'), 'changing your profile\'s privacy settings to public') . ', even if it\'s just for the event. Thanks!' ) }}
+@endif
 <div class="user-profile-header">
 	{{ HTML::userAvatar($user, 'large') }}
 	<h1>{{{ $user->username }}}</h1>
