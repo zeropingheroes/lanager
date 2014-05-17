f106611b8483fa837566f3e68cb56f66a5917473
ilumos <ilumos@gmail.com>
2014-05-17 04:11:03 +0100

    Upgraded to Laravel 4.1.28

--- app/models/User.php
+++ app/models/User.php
@@ -38,6 +38,47 @@ class User extends BaseModel implements UserInterface {
 		return $this->password;
 	}
 
+	/**
+	 * Get the token value for the "remember me" session.
+	 *
+	 * @return string
+	 */
+	public function getRememberToken()
+	{
+		return $this->remember_token;
+	}
+
+	/**
+	 * Set the token value for the "remember me" session.
+	 *
+	 * @param  string  $value
+	 * @return void
+	 */
+	public function setRememberToken($value)
+	{
+		$this->remember_token = $value;
+	}
+
+	/**
+	 * Get the column name for the "remember me" token.
+	 *
+	 * @return string
+	 */
+	public function getRememberTokenName()
+	{
+		return 'remember_token';
+	}
+
+	/**
+	 * Get the e-mail address where password reminders are sent.
+	 *
+	 * @return string
+	 */
+	public function getReminderEmail()
+	{
+		return $this->email;
+	}
+
 	public function roles()
 	{
 		return $this->belongsToMany('Zeropingheroes\Lanager\Models\Role');
