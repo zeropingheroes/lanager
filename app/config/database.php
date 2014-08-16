2581793a87ef1c2cddab4ba79c59eaf2aba2b0f7
ilumos <ilumos@gmail.com>
2014-08-16 22:49:27 +0100

    Tidied up database config file

--- app/config/database.php
+++ app/config/database.php
@@ -46,12 +46,6 @@ return array(
 
 	'connections' => array(
 
-		'sqlite' => array(
-			'driver'   => 'sqlite',
-			'database' => __DIR__.'/../database/production.sqlite',
-			'prefix'   => '',
-		),
-
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.0.0.1',
@@ -63,25 +57,31 @@ return array(
 			'prefix'    => '',
 		),
 
-		'pgsql' => array(
-			'driver'   => 'pgsql',
-			'host'     => 'localhost',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
-			'charset'  => 'utf8',
-			'prefix'   => '',
-			'schema'   => 'public',
-		),
+		// 'sqlite' => array(
+		// 	'driver'   => 'sqlite',
+		// 	'database' => __DIR__.'/../database/production.sqlite',
+		// 	'prefix'   => '',
+		// ),
 
-		'sqlsrv' => array(
-			'driver'   => 'sqlsrv',
-			'host'     => 'localhost',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
-			'prefix'   => '',
-		),
+		// 'pgsql' => array(
+		// 	'driver'   => 'pgsql',
+		// 	'host'     => 'localhost',
+		// 	'database' => 'database',
+		// 	'username' => 'root',
+		// 	'password' => '',
+		// 	'charset'  => 'utf8',
+		// 	'prefix'   => '',
+		// 	'schema'   => 'public',
+		// ),
+
+		// 'sqlsrv' => array(
+		// 	'driver'   => 'sqlsrv',
+		// 	'host'     => 'localhost',
+		// 	'database' => 'database',
+		// 	'username' => 'root',
+		// 	'password' => '',
+		// 	'prefix'   => '',
+		// ),
 
 	),
 
