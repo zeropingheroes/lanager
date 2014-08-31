a31c1d9324c53f82bdad8577bcf79f7fb3ba5e08
ilumos <ilumos@gmail.com>
2014-08-31 18:53:03 +0100

    Removed redundant validation lang entry

--- app/lang/en/validation.php
+++ app/lang/en/validation.php
@@ -82,7 +82,6 @@ return array(
 
 	'custom' => array(
 		'flood_protect'					=> 'You have posted too recently. Please wait a while and try again.',
-		'date_not_before_this_input'	=> 'The :attribute time must be after the :other time.',
 	),
 
 	/*
