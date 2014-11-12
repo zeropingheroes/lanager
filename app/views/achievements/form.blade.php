c5e11395b60c88f5e1b1304eab9b5908e3ef6ac2
ilumos <ilumos@gmail.com>
2014-11-12 22:35:48 +0000

    Fixing achievements views

--- app/views/achievements/form.blade.php
+++ app/views/achievements/form.blade.php
@@ -4,8 +4,6 @@
 {{ Form::label('description', 'Description') }}
 {{ Form::text('description',NULL,array('placeholder' => 'A description of how to attain the achievement, or what the achievement is')) }}
 
-{{ Form::label('visible', 'Visibility') }}
-{{ Form::labelled_checkbox('visible', 'Show the achievement even if it has not been achieved yet', '1', true) }}
-{{ Form::block_help('Make achievements that you don\'t want to encourage invisible.') }}
+{{ Form::hidden('visible', '1') }}
 
-{{ Button::submit('Submit') }}
+{{ Button::normal('Submit')->submit() }}
--- app/views/achievements/list.blade.php
+++ app/views/achievements/list.blade.php
@@ -1,12 +1,6 @@
 @extends('layouts.default')
 @section('content')
 	@if(count($achievements))
-		{{ Table::open(array('class' => 'achievements')) }}
-		@if( Authority::can('manage', 'achievements') )
-			{{ Table::headers('Name', 'Description', 'Achieved By', 'Controls') }}
-		@else
-			{{ Table::headers('Name', 'Description', 'Achieved By') }}
-		@endif
 		<?php
 		foreach( $achievements as $achievement )
 		{
@@ -16,9 +10,9 @@
 					'name'			=> e($achievement->name),
 					'descrption'	=> e($achievement->description),
 					'users'			=> $achievement->users->count(),
-					'controls'		=> 	Button::sm_link(URL::route('awards.create', array('achievement_id' => $achievement->id)), 'Award' )->withIcon(Icon::user())  . ' ' .
-										HTML::button('achievements.edit',$achievement->id, ['size' => 'sm']) .  
-										HTML::button('achievements.destroy',$achievement->id, ['size' => 'sm']),
+					'controls'		=> 	Button::normal( Icon::user() . 'Award' )->asLinkTo(URL::route('awards.create', array('achievement_id' => $achievement->id)), 'Award') .
+										HTML::button('achievements.edit', $achievement->id ) .  
+										HTML::button('achievements.destroy', $achievement->id),
 				);
 			}
 			else
@@ -34,15 +28,8 @@
 		{{ Table::withContents($tableBody) }}
 		
 		{{ $achievements->links() }}
-		<br>
-		@if( Authority::can('manage', 'achievements') )
-			@if( ! Input::get('hidden') )
-				{{ link_to_route('achievements.index', 'Show hidden achievements', array('hidden' => 1)) }}
-			@else
-				{{ link_to_route('achievements.index', 'Show all achievements', array('hidden' => 0)) }}
-				@endif
-		@endif
 	@else
-		No achievements found!
+		<p>No achievements found!</p>
 	@endif
+	{{ HTML::button('achievements.create') }}
 @endsection
