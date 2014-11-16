2198232702b12517fc0adb13001b982df43173c6
ilumos <ilumos@gmail.com>
2014-11-16 23:18:44 +0000

    Fixed migration rollback bug

--- app/migrations/2014_03_25_183319_add_playlist_playback_state.php
+++ app/migrations/2014_03_25_183319_add_playlist_playback_state.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPLaylistPlaybackState extends Migration {
+class AddPlaylistPlaybackState extends Migration {
 
 	/**
 	 * Run the migrations.
