41948af7470a6dc15e38d11bc7f2befb269edfd5
ilumos <ilumos@gmail.com>
2014-11-30 04:52:00 +0000

    Moved playlist max item duration into binding

--- app/Zeropingheroes/Lanager/Playlists/Items/ItemValidator.php
+++ app/Zeropingheroes/Lanager/Playlists/Items/ItemValidator.php
@@ -11,7 +11,7 @@ class ItemValidator extends ValidatorAssistant {
 		'user_id'		=> 'required|exists:users,id',
 		'url'			=> 'required|url|unique:playlist_items,url,{id},id,playlist_id,{playlist_id}',
 		'title'			=> 'required|max:255',
-		'duration'		=> 'required|numeric|min:1',
+		'duration'		=> 'required|numeric|min:1|max:{duration.max}',
 		'playback_state'=> 'in:0,1,2',
 	];
 
@@ -25,9 +25,9 @@ class ItemValidator extends ValidatorAssistant {
 		if( isset($this->inputs['id']) ) $this->bind('id', $this->inputs['id']);
 		$this->bind('playlist_id', $this->inputs['playlist_id']);
 		
-		// Inject the maximum permitted item duration
+		// Bind the maximum permitted item duration for use in rules
 		$playlist = Playlist::findOrFail( $this->inputs['playlist_id'] );
-		$this->rules['duration'] .= '|max:' . $playlist->max_item_duration;
+		$this->bind('duration.max', $playlist->max_item_duration);
 		$this->messages['duration.max'] = 'The item\'s duration must not exceed ' . Duration::longFormat($playlist->max_item_duration);
 
 	}
