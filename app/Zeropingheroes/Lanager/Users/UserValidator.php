d9f48dbc28310833223cef7292166608b98902a6
ilumos <ilumos@gmail.com>
2014-11-30 04:56:00 +0000

    Fixed UserValidator class name and namespace

--- app/Zeropingheroes/Lanager/Users/UserValidator.php
+++ app/Zeropingheroes/Lanager/Users/UserValidator.php
@@ -1,13 +1,12 @@
-<?php namespace Zeropingheroes\Lanager\Shouts;
+<?php namespace Zeropingheroes\Lanager\Users;
 
 use Fadion\ValidatorAssistant\ValidatorAssistant;
-use Auth, Carbon\Carbon;
 
-class ShoutValidator extends ValidatorAssistant {
+class UserValidator extends ValidatorAssistant {
 
 	protected $rules = [
 		'username'			=> 'required|max:32',
-		'steam_id_64'		=> 'required|max:17|unique:users',
+		'steam_id_64'		=> 'required|max:17',
 		'steam_visibility'	=> 'required|in:0,1,2,3',
 		'ip'				=> 'ip',
 		'avatar'			=> 'url',
