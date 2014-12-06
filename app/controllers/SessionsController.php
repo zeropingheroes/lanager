5eabd552d7e7726c1043f0740af5542af2d26a04
ilumos <ilumos@gmail.com>
2014-12-06 12:43:40 +0000

    Removed unused methods

--- app/controllers/SessionsController.php
+++ app/controllers/SessionsController.php
@@ -62,39 +62,6 @@ class SessionsController extends BaseController {
 	}
 
 	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
 	 * Remove the specified resource from storage.
 	 *
 	 * @param  int  $id
