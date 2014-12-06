5fb77bbd7bacc89024f49a3ffc50cf4488341fe1
ilumos <ilumos@gmail.com>
2014-12-06 12:31:35 +0000

    Added nullable field processing to BaseModel

--- app/Zeropingheroes/Lanager/BaseModel.php
+++ app/Zeropingheroes/Lanager/BaseModel.php
@@ -1,9 +1,39 @@
 <?php namespace Zeropingheroes\Lanager;
 
-use LaravelBook\Ardent\Ardent;
 use Zeropingheroes\Lanager\Users\User;
+use Eloquent;
 
-class BaseModel extends Ardent {
+class BaseModel extends Eloquent {
+
+	protected $nullable = [];
+
+	/**
+	* Listen for save event
+	*/
+	protected static function boot()
+	{
+		parent::boot();
+
+		static::saving(function($model)
+		{
+			self::setNullables($model);
+		});
+	}
+
+	/**
+	* Unset empty nullable fields so that MySQL will null/default them
+	* @param object $model
+	*/
+	protected static function setNullables($model)
+	{
+		foreach($model->nullable as $field)
+		{
+			if ( empty($model->{$field}) )
+			{
+				unset($model->{$field});
+			}
+		}
+	}
 
 	public function scopeOnlyVisibleUsers($query)
 	{
