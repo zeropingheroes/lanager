04eb7178f083ef502448b9e5ceaf99454a4fdba2
ilumos <ilumos@gmail.com>
2014-12-06 12:36:19 +0000

    Removing flood protect rule - awaiting fix

--- app/Zeropingheroes/Lanager/Shouts/ShoutValidator.php
+++ app/Zeropingheroes/Lanager/Shouts/ShoutValidator.php
@@ -11,19 +11,21 @@ class ShoutValidator extends ValidatorAssistant {
 		'pinned'		=> 'boolean',
 	];
 
-	protected $rulesStore = [
-		'content' => 'flood_protect:shouts',
-	];
+	// Awaiting issue response - https://github.com/fadion/ValidatorAssistant/issues/30
 
-	protected $messages = [
-		'content.flood_protect'	=> 'You have posted too recently - please wait a while and try again.',
-	];
+	// protected $rulesStore = [
+	// 	'content'		=> 'flood_protect:shouts',
+	// ];
 
-	public function customFloodProtect($attribute, $value, $parameters)
-	{
-		$date = new Carbon;
-		$date->subSeconds(15);
-		return ! Auth::user()->{$parameters[0]}()->where('created_at','>',$date)->count();
-	}
+	// protected $messages = [
+	// 	'content.flood_protect'	=> 'You have posted too recently - please wait a while and try again.',
+	// ];
+
+	// public function customFloodProtect($attribute, $value, $parameters)
+	// {
+	// 	$date = new Carbon;
+	// 	$date->subSeconds(15);
+	// 	return ! Auth::user()->{$parameters[0]}()->where('created_at','>',$date)->count();
+	// }
 
 }
