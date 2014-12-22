be55f471bd214b444771245659ce76e8741b197c
ilumos <ilumos@gmail.com>
2014-12-22 01:35:39 +0000

    Testing intval() validation filter

--- app/Zeropingheroes/Lanager/Pages/PageValidator.php
+++ app/Zeropingheroes/Lanager/Pages/PageValidator.php
@@ -4,6 +4,11 @@ use Fadion\ValidatorAssistant\ValidatorAssistant;
 
 class PageValidator extends ValidatorAssistant {
 
+	// Not yet implemented
+	// protected $filters = [
+	// 	'parent_id' => 'intval',
+	// ];
+
 	protected $rules = [
 		'title'		=> 'required|max:255',
 		'parent_id'	=> 'numeric|exists:pages,id',
