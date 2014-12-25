f1ddc0adecf80c696da685874f0b6e80d1eec787
ilumos <ilumos@gmail.com>
2014-12-25 01:02:21 +0000

    Fixed playback state validation

--- app/Zeropingheroes/Lanager/Playlists/PlaylistValidator.php
+++ app/Zeropingheroes/Lanager/Playlists/PlaylistValidator.php
@@ -7,6 +7,7 @@ class PlaylistValidator extends ValidatorAssistant {
 	protected $rules = [
 		'name'					=> 'required|max:255|unique:playlists,name,{id}',
 		'description'			=> 'max:255',
+		'playback_state'		=> 'in:0,1',
 		'max_item_duration'		=> 'numeric|min:1',
 		'user_skip_threshold'	=> 'numeric|min:1|max:100',
 	];
