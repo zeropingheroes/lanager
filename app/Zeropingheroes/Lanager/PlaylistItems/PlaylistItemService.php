09da649163b3af4b52904370aefc6e45fc62af80
ilumos <ilumos@gmail.com>
2015-02-10 20:47:22 +0000

    Moved controller logic to PlaylistItemService::store()

--- app/Zeropingheroes/Lanager/PlaylistItems/PlaylistItemService.php
+++ app/Zeropingheroes/Lanager/PlaylistItems/PlaylistItemService.php
@@ -3,6 +3,8 @@
 use Zeropingheroes\Lanager\NestedResourceService;
 use Zeropingheroes\Lanager\Playlists\Playlist;
 
+use Auth, Config;
+
 class PlaylistItemService extends NestedResourceService {
 
 	public $resource = 'playlist item';
@@ -16,4 +18,25 @@ class PlaylistItemService extends NestedResourceService {
 		parent::__construct($listener, $models);
 	}
 
+	public function store( array $ids, $input)
+	{
+		$input = array_only($input, ['url']); // filter out everything from the input but the URL
+		$input['user_id'] = Auth::user()->id;
+
+		$providers = Config::get('lanager/playlist.providers');
+
+		try // ...to pull in the item (by URL) from a provider
+		{
+			$playableItem = (new PlayableItemFactory)->create( $input['url'], $providers);
+			$input = array_merge($input, $playableItem->toArray());
+		}
+		catch(UnplayableItemException $e)
+		{
+			$this->errors = $e->getMessage();
+			return $this->listener->storeFailed( $this );
+		}
+
+		// pass input to default service provider
+		parent::store($ids, $input);
+	}
 }
