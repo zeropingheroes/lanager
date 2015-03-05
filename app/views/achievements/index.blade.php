941ea740a210325e37b22a277435234dda309511
ilumos <ilumos@gmail.com>
2015-03-05 22:41:22 +0000

    Polished achievements index page

--- app/views/achievements/index.blade.php
+++ app/views/achievements/index.blade.php
@@ -2,35 +2,51 @@
 @section('content')
 	@include('layouts.default.title')
 	@include('layouts.default.alerts')
+
 	@if(count($achievements))
-		<?php
-		foreach( $achievements as $achievement )
-		{
-			if( Authority::can('manage', 'achievements') )
-			{
-				$tableBody[] = array(
-					'name'				=> link_to_route('achievements.show', $achievement->name, $achievement->id),
-					'descrption'		=> e($achievement->description),
-					'achieved-count'	=> $achievement->userAchievements->count(),
-					'controls'			=> 	Button::normal( Icon::user() . 'Award' )->asLinkTo(URL::route('user-achievements.create', array('achievement_id' => $achievement->id)), 'Award') .
-											HTML::button('achievements.edit', $achievement->id ) .  
-											HTML::button('achievements.destroy', $achievement->id),
-				);
-			}
-			else
-			{
-				$tableBody[] = array(
-					'name'			=> e($achievement->name),
-					'descrption'	=> e($achievement->description),
-					'users'			=> $achievement->userAchievements->count(),
-				);	
-			}
-		}
-		?>
-		{{ Table::withContents($tableBody) }}
-		
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Description</th>
+					<th>Achieved By</th>
+					@if( Authority::can('manage', 'achievements') )
+						<th class="text-center">{{ Icon::cog() }}</th>
+					@endif
+				</tr>
+			</thead>
+			<tbody>
+			@foreach( $achievements as $achievement )
+				<tr>
+					<td>{{ link_to_route('achievements.show', $achievement->name, $achievement->id) }}</td>
+					<td>{{ $achievement->description }}</td>
+					<td>
+						@if( $achievement->userAchievements->count() > 0 )
+							{{ $achievement->userAchievements->count() }}
+							{{ str_plural('user', $achievement->userAchievements->count() ) }}
+						@endif
+					</td>
+					@if( Authority::can('manage', 'achievements') )
+						<td class="text-center">
+							@include(
+								'buttons.create',
+								[
+									'resource' => 'user-achievements',
+									'size' => 'extraSmall',
+									'icon' => 'user',
+									'hover' => 'Award this achievement',
+									'parameters' => ['achievement_id' => $achievement->id],
+								])
+							@include('buttons.edit', ['resource' => 'achievements', 'item' => $achievement, 'size' => 'extraSmall'])
+							@include('buttons.destroy', ['resource' => 'achievements', 'item' => $achievement, 'size' => 'extraSmall'])
+						</td>
+					@endif
+				</tr>
+			@endforeach
+			</tbody>
+		</table>
 	@else
 		<p>No achievements found!</p>
 	@endif
-	{{ HTML::button('achievements.create') }}
+	@include('buttons.create', ['resource' => 'achievements'])
 @endsection
