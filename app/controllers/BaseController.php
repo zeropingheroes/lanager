f54809608b347778c5e5c14fdf32acc54777ec59
ilumos <ilumos@gmail.com>
2015-03-05 18:48:17 +0000

    Removing redundant code

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -1,12 +1,10 @@
 <?php namespace Zeropingheroes\Lanager;
 
 use Illuminate\Routing\Controller;
-use Zeropingheroes\Lanager\BaseModel;
 use Zeropingheroes\Lanager\ResourceServiceListenerContract,
 	Zeropingheroes\Lanager\BaseResourceService,
 	Zeropingheroes\Lanager\Api\v1\Traits\WriteableResourceTrait;
 use Notification, Redirect;
-use ReflectionClass;
 
 class BaseController extends Controller implements ResourceServiceListenerContract {
 
@@ -17,54 +15,6 @@ class BaseController extends Controller implements ResourceServiceListenerContra
 	public function __construct()
 	{
 		$this->beforeFilter( 'permission' );
-	}
-
-	/**
-	 * Setup the layout used by the controller.
-	 *
-	 * @return void
-	 */
-	protected function setupLayout()
-	{
-		if ( ! is_null($this->layout))
-		{
-			$this->layout = View::make($this->layout);
-		}
-	}
-
-	protected function save( BaseModel $model )
-	{
-		$resource = $this->resourceName($model);
-		$action = isset($model->id) ? 'update' : 'store';
-
-		$validator = new $model->validator( $model->toArray() );
-		$validator->scope([$action]);
-
-		if ( $validator->fails() )
-		{
-			Notification::danger( $validator->errors()->all() );
-			return false;
-		}
-		else
-		{
-			$model->save();
-			Notification::success( trans('confirmation.after.resource.' . $action, ['resource' => trans('resources.' . $resource) ]) );
-			return true;			
-		}
-	}
-
-	protected function delete( BaseModel $model )
-	{
-		$resource = $this->resourceName($model);
-		$delete = $model->delete();
-		if( $delete ) Notification::success( trans('confirmation.after.resource.destroy', ['resource' => trans('resources.' . $resource) ]) );
-		return $delete;
-	}
-
-	private function resourceName( BaseModel $model )
-	{
-		$modelClass = (new ReflectionClass($model))->getShortName();
-		return strtolower(preg_replace('/([a-zA-Z])(?=[A-Z])/', '$1-', $modelClass)); // camel to dash
 	}
 
 	/*
