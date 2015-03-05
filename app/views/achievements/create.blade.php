2328b7fd0757889f85730b02b584d28623b3bea7
ilumos <ilumos@gmail.com>
2015-03-05 23:02:33 +0000

    Coding style changes

--- app/views/achievements/create.blade.php
+++ app/views/achievements/create.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 	@include('layouts.default.title')
 	@include('layouts.default.alerts')
+
 	{{ Form::model($achievement, ['route' => 'achievements.store']) }}
 		@include('achievements.partials.form')
 	{{ Form::close() }}
