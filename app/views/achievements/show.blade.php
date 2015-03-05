1915b61b10f375c9f38abc544103e619317d4168
ilumos <ilumos@gmail.com>
2015-03-05 22:57:56 +0000

    Polished achievement show page

--- app/views/achievements/show.blade.php
+++ app/views/achievements/show.blade.php
@@ -2,9 +2,40 @@
 @section('content')
 	@include('layouts.default.title')
 	@include('layouts.default.alerts')
+
 	<h4>{{{ $achievement->description }}}</h4>
-	@if( Authority::can('edit', 'achievement') )
-		{{ HTML::button('achievements.edit', $achievement->id) }}
-		{{ HTML::button('achievements.destroy', $achievement->id) }}
+
+	@if( $achievement->userAchievements->count() )
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Achievers</th>
+					<th>Awarded at</th>
+					@if( Authority::can('manage', 'user-achievements') )
+						<th class="text-center">{{ Icon::cog() }}</th>
+					@endif
+				</tr>
+			</thead>
+		@foreach($achievement->userAchievements as $userAchievement)
+			<tbody>
+				<tr>
+					<td>@include('users.partials.avatar-username', ['user' => $userAchievement->user])</td>
+					<td>{{ $userAchievement->lan->name }}</td>
+					@if( Authority::can('manage', 'user-achievements') )
+						<td class="text-center">
+							@include('buttons.edit', ['resource' => 'user-achievements', 'item' => $userAchievement, 'size' => 'extraSmall'])
+							@include('buttons.destroy', ['resource' => 'user-achievements', 'item' => $userAchievement, 'size' => 'extraSmall'])
+						</td>
+					@endif
+				</tr>
+			</tbody>
+		@endforeach
+		</table>
+	@else
+		<p>No-one has been awarded this achievement yet!</p>
 	@endif
+
+	@include('buttons.edit', ['resource' => 'achievements', 'item' => $achievement])
+	@include('buttons.destroy', ['resource' => 'achievements', 'item' => $achievement])
+
 @endsection
