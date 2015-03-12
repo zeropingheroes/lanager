055faf88a75b95760308ca710e035ea9d84a6bab
ilumos <ilumos@gmail.com>
2015-03-12 20:49:13 +0000

    Reverted avatar status display

--- app/views/users/partials/avatar.blade.php
+++ app/views/users/partials/avatar.blade.php
@@ -17,33 +17,31 @@
 	$classes[] = 'avatar';
 	$classes[] = 'avatar-'.$size;
 		
-	// $state = $user->state();
+	$state = $user->state();
 	
-	// if( $state )
-	// {
-	// 	if( isset($state->application_id) )
-	// 	{
-	// 		$classes[] = 'avatar-in-game';
-	// 		$title = 'In-Game: ' . $state->application->name;
-	// 	}
-	// 	elseif( $state->status )
-	// 	{
-	// 		$classes[] = 'avatar-online';
-	// 		$title = $state->present()->statusText;
-	// 	}
-	// 	else
-	// 	{
-	// 		$classes[] = 'avatar-offline';
-	// 		$title = $state->present()->statusText;
-	// 	}
-	// }
-	// else
-	// {
-	// 	$classes[] = 'avatar-offline';
-	// 	$title = 'Status unknown';
-	// }
+	if( $state )
+	{
+		if( isset($state->application_id) )
+		{
+			$classes[] = 'avatar-in-game';
+			$title = 'In-Game: ' . $state->application->name;
+		}
+		elseif( $state->status )
+		{
+			$classes[] = 'avatar-online';
+			$title = $state->present()->statusText;
+		}
+		else
+		{
+			$classes[] = 'avatar-offline';
+			$title = $state->present()->statusText;
+		}
+	}
+	else
+	{
 		$classes[] = 'avatar-offline';
 		$title = 'Status unknown';
+	}
 
 ?>
 <img class="{{{ implode(' ', $classes) }}}" src="{{ $url }}" alt="Avatar for {{{ $user->username }}}" title="{{{ $title }}}">
