02b68a77939a3ef10868e59f79be1ce9e38fa96d
ilumos <ilumos@gmail.com>
2015-03-15 14:42:26 +0000

    Added item count to playlist index

--- app/views/playlists/index.blade.php
+++ app/views/playlists/index.blade.php
@@ -10,6 +10,7 @@
 					<th>Name</th>
 					<th>Description</th>
 					<th>Status</th>
+					<th>Items</th>
 					@if( Authority::can('manage', 'playlists') )
 						<th class="text-center">{{ Icon::cog() }}</th>
 					@endif
@@ -18,9 +19,18 @@
 			<tbody>
 			@foreach( $playlists as $playlist )
 				<tr>
-					<td>{{ link_to_route('playlists.items.index', $playlist->name, $playlist->id) }}</td>
-					<td>{{ $playlist->description }}</td>
-					<td>@include('playlists.partials.status', ['playlist' => $playlist])</td>
+					<td>
+						{{ link_to_route('playlists.items.index', $playlist->name, $playlist->id) }}
+					</td>
+					<td>
+						{{ $playlist->description }}
+					</td>
+					<td>
+						@include('playlists.partials.status', ['playlist' => $playlist])
+					</td>
+					<td>
+						@include('plural', ['singular' => 'item', 'collection' => $playlist->playlistItems ])
+					</td>
 					@if( Authority::can('manage', 'playlists') )
 						<td class="text-center">
 							@include('buttons.edit', ['resource' => 'playlists', 'item' => $playlist, 'size' => 'extraSmall'])
