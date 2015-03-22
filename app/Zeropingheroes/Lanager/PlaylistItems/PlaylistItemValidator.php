a0318477cceabee5957b37b2d86e288324e30a0c
ilumos <ilumos@gmail.com>
2015-03-22 16:26:55 +0000

    Adding 2 video per user per hour limit to playlist item validation

--- app/Zeropingheroes/Lanager/PlaylistItems/PlaylistItemValidator.php
+++ app/Zeropingheroes/Lanager/PlaylistItems/PlaylistItemValidator.php
@@ -2,7 +2,8 @@
 
 use Fadion\ValidatorAssistant\ValidatorAssistant;
 use Zeropingheroes\Lanager\Playlists\Playlist;
-use Duration;
+use Zeropingheroes\Lanager\PlaylistItems\PlaylistItem;
+use Duration, Carbon\Carbon;;
 
 class PlaylistItemValidator extends ValidatorAssistant {
 
@@ -17,8 +18,13 @@ class PlaylistItemValidator extends ValidatorAssistant {
 		'skip_reason'	=> 'max:255',
 	];
 
+	protected $rulesStore = [
+		'playlist_id'	=> 'submissionsPerHour:2',
+	];
+
 	protected $messages = [
 		'url.unique'	=> 'The item has already been added to this playlist',
+		'submissions_per_hour'	=> 'You have submitted 2 items in the past hour - the maximum allowed. Please wait a while and try again later.',
 	];
 
 	protected function before()
@@ -26,11 +32,22 @@ class PlaylistItemValidator extends ValidatorAssistant {
 		// Bind item id and playlist id for use in rules
 		if( isset($this->inputs['id']) ) $this->bind('id', $this->inputs['id']);
 		$this->bind('playlist_id', $this->inputs['playlist_id']);
-		
+	
 		// Bind the maximum permitted item duration for use in rules
 		$playlist = Playlist::findOrFail( $this->inputs['playlist_id'] );
 		$this->bind('duration.max', $playlist->max_item_duration);
 		$this->messages['duration.max'] = 'The item\'s duration must not exceed ' . Duration::longFormat($playlist->max_item_duration);
-
 	}
+
+	public function customSubmissionsPerHour($attribute, $value, $parameters)
+	{
+		$oneHourAgo = (new Carbon)->subSeconds(3600);
+
+		$submittedInLastHour = PlaylistItem::where( 'playlist_id', $this->inputs['playlist_id'] )
+											->where( 'user_id', $this->inputs['user_id'] )
+											->where('created_at','>',$oneHourAgo)
+											->count();
+		return $submittedInLastHour < $parameters[0];
+	}
+
 }
