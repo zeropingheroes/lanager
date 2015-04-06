62e5a36b5d99f60d8d0e9546ccaeaf8cac09f189
ilumos <ilumos@gmail.com>
2015-04-06 21:25:02 +0100

    Removed seconds from event datetimepicker

--- app/Zeropingheroes/Lanager/Events/EventValidator.php
+++ app/Zeropingheroes/Lanager/Events/EventValidator.php
@@ -6,10 +6,10 @@ class EventValidator extends ValidatorAssistant {
 
 	protected $rules = [
 		'name'			=> 'required|max:255',
-		'start'			=> 'required|date_format:Y-m-d H:i:s|before:end',
-		'end'			=> 'required|date_format:Y-m-d H:i:s|after:start',
-		'signup_opens'	=> 'date_format:Y-m-d H:i:s|before:signup_closes|before:end',
-		'signup_closes'	=> 'date_format:Y-m-d H:i:s|after:signup_opens',
+		'start'			=> 'required|date_format:Y-m-d H:i|before:end',
+		'end'			=> 'required|date_format:Y-m-d H:i|after:start',
+		'signup_opens'	=> 'date_format:Y-m-d H:i|before:signup_closes|before:end',
+		'signup_closes'	=> 'date_format:Y-m-d H:i|after:signup_opens',
 		'event_type_id'	=> 'numeric|exists:event_types,id',
 	];
 
