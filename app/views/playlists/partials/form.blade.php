2b396300c15ba09e3e9b781ac598f97cf255b39a
ilumos <ilumos@gmail.com>
2015-04-12 00:11:39 +0100

    Increasing size of playlist form

--- app/views/playlists/partials/form.blade.php
+++ app/views/playlists/partials/form.blade.php
@@ -2,8 +2,8 @@
 	Form::label('name', 'Name'),
 	Form::text('name',NULL, ['placeholder' => 'The name of the playlist', 'maxlength' => 255] ),
 	NULL,
-	4,
-	4
+	2,
+	9
 )->withAttributes( ['class' => 'required'] )
 }}
 
@@ -11,28 +11,28 @@
 	Form::label('description', 'Description'),
 	Form::text('description',NULL, ['placeholder' => 'A brief description of the playlist', 'maxlength' => 255] ),
 	NULL,
-	4,
-	4
+	2,
+	9
 ) }}
 
 {{ ControlGroup::generate(
 	Form::label('max_item_duration', 'Max Item Duration'),
 	InputGroup::withContents(Form::text('max_item_duration', 600))->append('seconds'),
 	NULL,
-	4,
-	4
+	2,
+	9
 ) }}
 
 {{ ControlGroup::generate(
 	Form::label('user_skip_threshold', 'User Skip Threshold'),
 	InputGroup::withContents(Form::text('user_skip_threshold', 50))->append('%'),
 	Form::help('Active users required to vote to skip a playlist item'),
-	4,
-	4
+	2,
+	9
 ) }}
 
 <div class="row">
-	<div class="col-md-2 col-md-offset-4">
+	<div class="col-md-2 col-md-offset-2">
 		{{ Button::normal('Submit')->submit() }}
 	</div>
 </div>
