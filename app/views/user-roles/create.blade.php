be48ea333620c7f97de84e86fdb052929f68da88
ilumos <ilumos@gmail.com>
2015-04-12 00:44:19 +0100

    Changing user role form to be horizontal form

--- app/views/user-roles/create.blade.php
+++ app/views/user-roles/create.blade.php
@@ -3,7 +3,7 @@
 	@include('layouts.default.title')
 	@include('layouts.default.alerts')
 
-	{{ Form::model($userRole, ['route' => 'user-roles.store']) }}
+	{{ Form::model($userRole, ['route' => 'user-roles.store', 'class' => 'form-horizontal']) }}
 		@include('user-roles.partials.form')
 	{{ Form::close() }}
 
--- app/views/user-roles/partials/form.blade.php
+++ app/views/user-roles/partials/form.blade.php
@@ -1,7 +1,23 @@
-{{ Form::label('user_id', 'User') }}
-{{ Form::select('user_id', $users, Input::get('user_id')) }}
+{{ ControlGroup::generate(
+	Form::label('user_id', 'User'),
+	Form::select('user_id', $users, Input::get('user_id')),
+	NULL,
+	2,
+	9
+)->withAttributes( ['class' => 'required'] )
+}}
 
-{{ Form::label('role_id', 'Role') }}
-{{ Form::select('role_id', $roles, Input::get('role_id')) }}
+{{ ControlGroup::generate(
+	Form::label('role_id', 'Role'),
+	Form::select('role_id', $roles, Input::get('role_id')),
+	NULL,
+	2,
+	9
+)->withAttributes( ['class' => 'required'] )
+}}
 
-{{ Button::normal('Submit')->submit() }}
+<div class="row">
+	<div class="col-md-2 col-md-offset-2">
+		{{ Button::normal('Submit')->submit() }}
+	</div>
+</div>
