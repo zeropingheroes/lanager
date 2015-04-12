23943074d1b711a2dc0a59e13e2010cc891e5da8
ilumos <ilumos@gmail.com>
2015-04-12 05:39:30 +0100

    Added docblocks to base GUI controller

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -27,36 +27,55 @@ class BaseController extends Controller implements ResourceServiceListenerContra
 	| These methods can be overridden by child controllers if needed.
 	|
 	*/
+	/**
+	 * Display HTML notification and redirect based on service action result
+	 * @param  BaseResourceService $service Service class that was called
+	 */
 	public function storeSucceeded( BaseResourceService $service )
 	{
 		Notification::success( $service->messages() );
 		return Redirect::route( $this->route . '.show', $service->resourceIds() );
 	}
 
+	/**
+	 * @see BaseController::storeSucceeded
+	 */
 	public function storeFailed( BaseResourceService $service )
 	{
 		Notification::danger( $service->errors() );
 		return Redirect::back()->withInput();
 	}
 
+	/**
+	 * @see BaseController::storeSucceeded
+	 */
 	public function updateSucceeded( BaseResourceService $service )
 	{
 		Notification::success( $service->messages() );
 		return Redirect::route( $this->route . '.show', $service->resourceIds() );
 	}
 
+	/**
+	 * @see BaseController::storeSucceeded
+	 */
 	public function updateFailed( BaseResourceService $service )
 	{
 		Notification::danger( $service->errors() );
 		return Redirect::back()->withInput();
 	}
 
+	/**
+	 * @see BaseController::storeSucceeded
+	 */
 	public function destroySucceeded( BaseResourceService $service )
 	{
 		Notification::success( $service->messages() );
 		return Redirect::route( $this->route . '.index', $service->resourceIds() );
 	}
 
+	/**
+	 * @see BaseController::storeSucceeded
+	 */
 	public function destroyFailed( BaseResourceService $service )
 	{
 		Notification::danger( $service->errors() );
