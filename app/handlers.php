020a8d1ab0b458593c492bb15e9aca548ad8e166
ilumos <ilumos@gmail.com>
2015-04-19 01:22:31 +0100

    Improving service logging

--- app/handlers.php
+++ app/handlers.php
@@ -18,7 +18,7 @@ Event::listen('lanager.services.*', function( $parameters )
 {
 	$eventName = str_replace( 'lanager.services.', '', Event::firing() );
 
-	$username = is_object( Auth::user() ) ? Auth::user()->username : 'Guest';
+	$username = is_object( Auth::user() ) ? Auth::user()->username : 'guest';
 
-	Log::debug( $logMessage , $username . ': ' . $eventName );
+	Log::debug( e($username) . ': ' . $eventName , $parameters );
 });
