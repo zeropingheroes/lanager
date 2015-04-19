4e90f9afc056b4c49b8a4318448a985531bbdf18
ilumos <ilumos@gmail.com>
2015-04-19 01:22:49 +0100

    Adding logging to user handler

--- app/Zeropingheroes/Lanager/Users/UserHandler.php
+++ app/Zeropingheroes/Lanager/Users/UserHandler.php
@@ -1,6 +1,7 @@
 <?php namespace Zeropingheroes\Lanager\Users;
 
 use Zeropingheroes\Lanager\Roles\Role;
+use Log;
 
 class UserHandler {
 
@@ -12,7 +13,7 @@ class UserHandler {
 	*/
 	public function subscribe($events)
 	{
-		$events->listen('lanager.services.users.store', 'Zeropingheroes\Lanager\Users\UserHandler@onStore');
+		$events->listen('lanager.users.store', 'Zeropingheroes\Lanager\Users\UserHandler@onStore');
 	}
 
 	/**
@@ -22,13 +23,18 @@ class UserHandler {
 	public function onStore($user)
 	{
 		// Make the first user SuperAdmin
-		if( count(User::all()) == 1 && ! $user->hasRole('Super Admin') )	$user->roles()->attach(Role::where('name', '=', 'Super Admin')->firstOrFail());
+		if( count(User::all()) == 1 && ! $user->hasRole('Super Admin') )
+		{
+			$user->roles()->attach(Role::where('name', '=', 'Super Admin')->firstOrFail());
+			Log::debug( e($user->username) . ': Assigning "Super Admin" role as first user to log in', [$user->api_key]);
+		}
 
 		// Generate an API key if the user does not have one
 		if( empty($user->api_key) )
 		{
 			$user->api_key = md5(str_random(32));
 			$user->save();
+			Log::debug( e($user->username) . ': Generating API key', [$user->api_key]);
 		}
 	}
 
