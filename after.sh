35d0a6aca9dd2310354d83a5ca37de8d4d46cc2d
ilumos <ilumos@gmail.com>
2018-04-07 12:42:26 +0100

    Create DB structure and seed on Vagrant up

--- after.sh
+++ after.sh
@@ -3,3 +3,5 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+cd /home/vagrant/lanager/ && php artisan migrate:fresh && php artisan db:seed
