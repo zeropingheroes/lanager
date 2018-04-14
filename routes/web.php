c64b45b7519c450c32ce1eaa9e6fad8cfd35de7d
ilumos <ilumos@gmail.com>
2018-04-14 23:39:35 +0100

    Prevented logged in users from accessing login page

--- routes/web.php
+++ routes/web.php
@@ -5,22 +5,28 @@
  */
 Route::get('/', function () {
     return redirect()->route('users.index');
-});
+})->name('home');
 
 /**
- * Login & Logout
+ * Login
  */
-// TODO: redirect logged in users to home
 Route::get('login', 'AuthController@showLoginForm')
+    ->middleware(['guest'])
     ->name('login');
 
 Route::get('auth/{provider}', 'AuthController@redirectToProvider')
+    ->middleware(['guest'])
     ->name('auth');
 
 Route::get('auth/{provider}/callback', 'AuthController@handleProviderCallback')
+    ->middleware(['guest'])
     ->name('auth.callback');
 
+/**
+ * Logout
+ */
 Route::post('logout', 'AuthController@logout')
+    ->middleware(['auth'])
     ->name('logout');
 
 /**
