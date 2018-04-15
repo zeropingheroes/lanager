470c3cc31a4908fa92ff332f8460c3d2594ea1ab
ilumos <ilumos@gmail.com>
2018-04-15 04:48:00 +0100

    Adding Steam states and servers tables

--- database/migrations/2018_04_14_235006_steam_tables_create.php
+++ database/migrations/2018_04_14_235006_steam_tables_create.php
@@ -22,6 +22,70 @@ class SteamTablesCreate extends Migration
             $table->primary('id');
         });
 
+        Schema::create('steam_app_servers', function ($table) {
+
+            $table->increments('id');
+
+            $table->integer('steam_app_id')
+                ->unsigned();
+
+            $table->string('address');    // ipv4, hostname, or ipv6
+
+            $table->integer('port')
+                ->nullable();
+
+            $table->timestamps();
+
+            // Relationships
+            $table->foreign('steam_app_id')
+                ->references('id')
+                ->on('steam_apps')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+
+        Schema::create('steam_user_states', function ($table) {
+
+            $table->increments('id');
+
+            $table->integer('user_id')
+                ->unsigned();
+
+            $table->integer('steam_app_id')
+                ->unsigned()
+                ->nullable(); // nullable to prevent circular dependencies
+
+            $table->integer('steam_app_server_id')
+                ->unsigned()
+                ->nullable();  // nullable to prevent circular dependencies
+
+            $table->smallInteger('online_status')
+                ->unsigned();
+
+            $table->timestamps();
+
+            // Relationships
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->foreign('steam_app_id')
+                ->references('id')
+                ->on('steam_apps')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+
+            $table->foreign('steam_app_server_id')
+                ->references('id')
+                ->on('steam_app_servers')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+
+            // Indexes
+            $table->index(['user_id', 'created_at']);
+        });
     }
 
     /**
@@ -31,6 +95,8 @@ class SteamTablesCreate extends Migration
      */
     public function down()
     {
+        Schema::drop('steam_user_states');
+        Schema::drop('steam_app_servers');
         Schema::drop('steam_apps');
     }
 }
