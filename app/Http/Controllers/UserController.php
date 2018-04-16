e85ca421bbb8d3946114c659c87274198bcab6f4
ilumos <ilumos@gmail.com>
2018-04-16 21:40:23 +0100

    Sorting user list by username

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::orderBy('username')->get();
         return View::make('pages.user.index')
             ->with('users', $users);
     }
