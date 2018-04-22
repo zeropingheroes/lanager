3458e63a186d4d3a8169e64b8d79245e5206fed4
ilumos <ilumos@gmail.com>
2018-04-22 12:14:24 +0100

    Adding error display to admin menu

--- resources/views/layouts/partials/nav/admin.blade.php
+++ resources/views/layouts/partials/nav/admin.blade.php
@@ -1,6 +1,7 @@
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" id="admin-menu" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <span class="oi oi-cog" title="Cog" aria-hidden="true"></span>
+        @if($errorCount)<span class="badge badge-danger align-text-top">{{ $errorCount }}</span>@endif
     </a>
     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
         <a class="dropdown-item" href="{{ route('logs.index') }}">@lang('title.logs') @if($errorCount) <span class="badge badge-danger">{{ $errorCount }}</span>@endif</a>
