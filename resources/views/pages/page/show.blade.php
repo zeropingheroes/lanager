69e90ff1f3f653aacc4e095556410f95fbff1029
ilumos <ilumos@gmail.com>
2018-05-10 16:41:14 +0100

    Adding display of child pages to parent page

--- resources/views/pages/page/show.blade.php
+++ resources/views/pages/page/show.blade.php
@@ -8,6 +8,15 @@
     <h1>{{ $page->title }}</h1>
     @include('components.alerts')
     {!! Markdown::convertToHtml($page->content) !!}
+
+    @if (!empty($page->children))
+        <ul>
+        @foreach($page->children as $child)
+            <li>{{ link_to_route('pages.show',$child->title, $child->id) }}</li>
+        @endforeach
+        </ul>
+    @endif
+
     @can('update', $page)
         @include('components.edit', ['route' => route('pages.edit', $page->id)])
     @endcan
