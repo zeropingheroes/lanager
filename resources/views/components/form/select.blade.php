2f993a494932ee3594367c9813e3a73383030d2f
ilumos <ilumos@gmail.com>
2018-05-10 18:26:36 +0100

    Moved more into select view partial

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -1,5 +1,11 @@
+@php
+    $selected = $selected ?? old($name, $item->{$name});
+    $valueField = $valueField ?? 'id';
+    $options = $options ?? $items->pluck($labelField, $valueField)->prepend('','');
+@endphp
 <select class="custom-select" id="{{ $name }}" name="{{ $name }}">
     @foreach($options as $value => $label)
         <option @if($value == $selected) selected @endif value="{{ $value }}">{{ $label }}</option>
     @endforeach
 </select>
+
--- resources/views/pages/page/partials/form.blade.php
+++ resources/views/pages/page/partials/form.blade.php
@@ -1,13 +1,12 @@
-@php
-    $parentSelect = $pages->pluck('title', 'id')->prepend('','');
-@endphp
 <div class="form-group">
     <label for="title">@lang('title.title')</label>
-    <input type="text" class="form-control" id="title" name="title" placeholder="@lang('title.title')" value="{{ old('title', $page->title) }}">
+    <input type="text" class="form-control" id="title" name="title" placeholder="@lang('title.title')"
+           value="{{ old('title', $page->title) }}">
 </div>
 <div class="form-group">
     <label for="content">@lang('title.content')</label>
-    <textarea class="form-control" id="content" name="content" rows="10" placeholder="@lang('title.content')" aria-describedby="contentHelp">{{ old('content', $page->content) }}</textarea>
+    <textarea class="form-control" id="content" name="content" rows="10" placeholder="@lang('title.content')"
+              aria-describedby="contentHelp">{{ old('content', $page->content) }}</textarea>
     <small id="contentHelp" class="form-text text-muted">
         <a href="@lang('phrase.markdown-formatting-help-link-url')" target="_blank">@lang('phrase.markdown-formatting-help-link')</a>
         @lang('phrase.content-help')
@@ -15,11 +14,12 @@
 </div>
 <div class="form-group">
     <label for="parent_id">@lang('title.parent')</label>
-    @include('components.form.select', ['name' => 'parent_id', 'options' => $parentSelect, 'selected' => old('parent_id', $page->parent_id)])
+    @include('components.form.select', ['name' => 'parent_id', 'item' => $page, 'items' => $pages, 'labelField' => 'title'])
 </div>
 <div class="form-group">
     <div class="custom-control custom-checkbox">
-        <input type="checkbox" class="custom-control-input" id="published" name="published" value="1" {{ old('published', $page->published) ? 'checked' : null}}>
+        <input type="checkbox" class="custom-control-input" id="published" name="published"
+               value="1" {{ old('published', $page->published) ? 'checked' : null}}>
         <label class="custom-control-label" for="published">@lang('title.published')</label>
     </div>
 </div>
