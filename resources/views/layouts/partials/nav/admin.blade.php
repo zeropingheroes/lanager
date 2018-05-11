b0661002419a1ad36074b3dfd6ee5ffffd137ecc
ilumos <ilumos@gmail.com>
2018-05-12 00:16:54 +0100

    Removing unnecessary links from admin nav menu

--- resources/views/layouts/partials/nav/admin.blade.php
+++ resources/views/layouts/partials/nav/admin.blade.php
@@ -6,8 +6,5 @@
     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
         <a class="dropdown-item" href="{{ route('logs.index') }}">@lang('title.logs') @if($errorCount) <span class="badge badge-danger">{{ $errorCount }}</span>@endif</a>
         <a class="dropdown-item" href="{{ route('role-assignments.index') }}">@lang('title.roles')</a>
-        <a class="dropdown-item" href="#">@lang('title.events')</a>
-        <a class="dropdown-item" href="#">@lang('title.info')</a>
-        <a class="dropdown-item" href="#">@lang('title.achievements')</a>
     </div>
 </li>
