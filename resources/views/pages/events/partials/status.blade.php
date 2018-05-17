09794d39e50ba235d6038edf086ef9454288529d
ilumos <ilumos@gmail.com>
2018-05-17 22:46:50 +0200

    Adding colours to event status badge

--- resources/views/pages/events/partials/status.blade.php
+++ resources/views/pages/events/partials/status.blade.php
@@ -4,15 +4,19 @@
 
     if ($start->isFuture() && $end->isFuture()) {
         $status = __('phrase.upcoming');
+        $class = 'future';
     } elseif ($start->isPast() && $end->isFuture()) {
         $status = __('phrase.happening-now');
+        $class = 'present';
     } elseif ($start->isPast() && $end->isPast()) {
         $status = __('phrase.ended');
+        $class = 'past';
     } else {
         $status = __('phrase.unknown');
+        $class = 'secondary';
     }
 @endphp
 
-<span class="badge badge-secondary" title="@include('pages.events.partials.status-relative', ['event' => $event])">
+<span class="badge badge-{{ $class }}" title="@include('pages.events.partials.status-relative', ['event' => $event])">
     {{ $status }}
 </span>
