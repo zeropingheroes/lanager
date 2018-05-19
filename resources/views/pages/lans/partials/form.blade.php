919e0664949ad6e7842f0f3c4eb98fea2b27541e
ilumos <ilumos@gmail.com>
2018-05-19 16:00:45 +0100

    Revert "Moving date time pickers to view partial"
    This reverts commit c10e7b0a1d0b99cc726c67156c1c6bdce9246a0b.

--- resources/views/pages/lans/partials/form.blade.php
+++ resources/views/pages/lans/partials/form.blade.php
@@ -3,7 +3,55 @@
     <input type="text" class="form-control" id="name" name="name" placeholder="@lang('title.name')"
            value="{{ old('name', $lan->name) }}">
 </div>
-@include('components.form.date-time-picker-pair', ['item' => $lan, 'startFieldName' => 'start', 'endFieldName' => 'end'])
+<div class="form-row">
+    <script type="text/javascript">
+        $(function () {
+            var format = 'YYYY-MM-DD HH:mm:ss';
+
+            var start = $('#start');
+            var end = $('#end');
+            var startDate = moment(start.val(), format).toDate();
+            var endDate = moment(end.val(), format).toDate();
+
+            start.datetimepicker({
+                format: format,
+                date: moment(),
+                sideBySide: true
+            });
+
+            start.datetimepicker('date', startDate);
+
+            end.datetimepicker({
+                format: format,
+                date: moment(),
+                sideBySide: true,
+                useCurrent: false
+            });
+
+            end.datetimepicker('date', endDate);
+
+            $("#start").on("change.datetimepicker", function (e) {
+                $('#end').datetimepicker('minDate', e.date);
+            });
+            $("#end").on("change.datetimepicker", function (e) {
+                $('#start').datetimepicker('maxDate', e.date);
+            });
+        });
+    </script>
+    <div class="form-group col-md-6">
+        <label for="start">@lang('title.start')</label>
+        <input type="text" class="form-control datetimepicker-input" id="start" name="start"
+               placeholder="YYYY-MM-DD HH:MM:SS" value="{{ old('start', $lan->start) }}"
+               data-toggle="datetimepicker" data-target="#start">
+    </div>
+
+    <div class="form-group col-md-6">
+        <label for="end">@lang('title.end')</label>
+        <input type="text" class="form-control datetimepicker-input" id="end" name="end"
+               placeholder="YYYY-MM-DD HH:MM:SS" value="{{ old('end', $lan->end) }}"
+               data-toggle="datetimepicker" data-target="#end">
+    </div>
+</div>
 <div class="form-group">
     <div class="custom-control custom-checkbox">
         <input type="checkbox" class="custom-control-input" id="published" name="published"
