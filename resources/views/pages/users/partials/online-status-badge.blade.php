942f2249f3cf47409166ef871e14fa67ee65d889
ilumos <ilumos@gmail.com>
2018-05-24 20:29:08 +0100

    Adding i18n to user status badge

--- resources/views/pages/users/partials/online-status-badge.blade.php
+++ resources/views/pages/users/partials/online-status-badge.blade.php
@@ -1,30 +1,33 @@
+@php
+    $statusText = __('phrase.status-'.str_replace(' ', '-', strtolower($user->state->status())));
+@endphp
 @switch(strtoupper($user->state->status()))
     @case('ONLINE')
-    <span class="badge badge-info">{{ $user->state->status() }}</span>
+    <span class="badge badge-info">{{ $statusText }}</span>
     @break
 
     @case('IN GAME')
-    <span class="badge badge-success">{{ $user->state->status() }}</span>
+    <span class="badge badge-success">{{ $statusText }}</span>
     @break
 
     @case('BUSY')
-    <span class="badge badge-danger">{{ $user->state->status() }}</span>
+    <span class="badge badge-danger">{{ $statusText }}</span>
     @break
 
     @case('AWAY')
     @case('SNOOZE')
-    <span class="badge badge-warning">{{ $user->state->status() }}</span>
+    <span class="badge badge-warning">{{ $statusText }}</span>
     @break
 
     @case('LOOKING TO TRADE')
     @case('LOOKING TO PLAY')
-    <span class="badge badge-info">{{ $user->state->status() }}</span>
+    <span class="badge badge-info">{{ $statusText }}</span>
     @break
 
     @case('OFFLINE')
-    <span class="badge badge-secondary">{{ $user->state->status() }}</span>
+    <span class="badge badge-secondary">{{ $statusText }}</span>
     @break
 
     @default
-    <span class="badge badge-secondary">{{ $user->state->status() }}</span>
+    <span class="badge badge-secondary">{{ $statusText }}</span>
 @endswitch
