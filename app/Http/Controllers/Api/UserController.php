58171215aaf31e635d921f1daee711ddb3d52cf6
ilumos <ilumos@gmail.com>
2018-05-30 17:47:16 +0100

    Adding user index API endpoint

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        return UserResource::collection(User::orderBy('username')->get());
     }
 
     /**
