36c7ce9f84c8213f6a76f1639a4f0c6f7c882f2d
ilumos <ilumos@gmail.com>
2018-08-12 12:58:06 +0100

    Fixing navbar collapse position and background bug

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,29 +1,24 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            <img src="/img/brand.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-            {{ config('app.name') }}
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="@lang('phrase.toggle-navigation')">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-            @include('layouts.partials.nav.primary')
-
-            <ul class="nav navbar-nav navbar-right">
-                <!-- Authentication Links -->
-                @guest
-                    @include('layouts.partials.nav.guest')
-                @else
-                    @if (Auth::user()->hasRole('Super Admin'))
+    <a class="navbar-brand" href="{{ url('/') }}">
+        <img src="/img/brand.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        {{ config('app.name') }}
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="@lang('phrase.toggle-navigation')">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        @include('layouts.partials.nav.primary')
+        <ul class="nav navbar-nav navbar-right">
+            <!-- Authentication Links -->
+            @guest
+                @include('layouts.partials.nav.guest')
+            @else
+                @if (Auth::user()->hasRole('Super Admin'))
                         @include('layouts.partials.nav.admin')
-                    @endif
-                    @include('layouts.partials.nav.user')
-                @endguest
-            </ul>
-        </div>
+                @endif
+                @include('layouts.partials.nav.user')
+            @endguest
+        </ul>
     </div>
 </nav>
