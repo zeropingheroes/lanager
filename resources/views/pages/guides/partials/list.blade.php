69409a9611052f5ba9121dbe1eb5bea4feb9ea11
ilumos <ilumos@gmail.com>
2018-08-18 11:07:56 +0100

    Moving guide actions to top right of page

--- resources/views/pages/guides/partials/list.blade.php
+++ resources/views/pages/guides/partials/list.blade.php
@@ -14,28 +14,7 @@
                 <td>
                     @lang('title.updated') @include('components.time-relative', ['datetime' => $guide->updated_at])
                 </td>
-                @canany(['update', 'delete'], $guide)
-                    <td>
-                        @component('components.actions-dropdown')
-                            @can('update', $guide)
-                                <a class="dropdown-item copy-markdown"
-                                   href="#"
-                                   data-clipboard-text="[{{ $guide->title }}]({{ route('lans.guides.show', ['lan' => $guide->lan, 'guide' => $guide, 'slug' => str_slug($guide->title)], false) }})">
-                                    @lang('title.copy-markdown-link')
-                                </a>
-                                <a href="{{ route('lans.guides.edit', ['lan' => $guide->lan, 'guide' => $guide]) }}"
-                                   class="dropdown-item">@lang('title.edit')</a>
-                            @endcan
-                            @can('delete', $guide)
-                                <form action="{{ route('lans.guides.destroy', ['lan' => $guide->lan, 'guide' => $guide]) }}" method="POST">
-                                    {{ method_field('DELETE') }}
-                                    {{ csrf_field() }}
-                                    <a class="dropdown-item" href="#" onclick="$(this).closest('form').submit();">@lang('title.delete')</a>
-                                </form>
-                            @endcan
-                        @endcomponent
-                    </td>
-                @endcanany
+                @include('pages.guides.partials.actions-dropdown', ['guide' => $guide])
             </tr>
         @endcan
     @endforeach
--- resources/views/pages/guides/show.blade.php
+++ resources/views/pages/guides/show.blade.php
@@ -5,7 +5,16 @@
 @endsection
 
 @section('content')
-    <h1>{{ $guide->title }}</h1>
+    <div class="row align-items-center">
+        <div class="col-md-auto">
+            <h1>{{ $guide->title }}</h1>
+        </div>
+        <div class="col text-right">
+            @include('pages.guides.partials.actions-dropdown', ['guide' => $guide])
+        </div>
+    </div>
+
+
     {{ Breadcrumbs::render('lans.guides.show', $lan, $guide) }}
 
     @include('components.alerts.all')
@@ -22,16 +31,5 @@
     @endif
 
     {!! Markdown::convertToHtml($guide->content) !!}
-
-    @can('update', $guide)
-        <a href="{{ route( 'lans.guides.edit', ['lan' => $lan, 'guide' => $guide]) }}" class="btn btn-primary">@lang('title.edit')</a>
-    @endcan
-    @can('delete', $guide)
-        <form action="{{ route( 'lans.guides.destroy', ['lan' => $lan, 'guide' => $guide]) }}" method="POST" class="inline">
-            {{ method_field('DELETE') }}
-            {{ csrf_field() }}
-            <button type="submit" class="btn btn-danger">@lang('title.delete')</button>
-        </form>
-    @endcan
 
 @endsection
