564f5e7f597b83eb8291ef2190e7599aa43ecf49
ilumos <ilumos@gmail.com>
2018-11-04 00:29:49 +0000

    Adding LANs to admin menu

--- resources/views/layouts/partials/nav/admin.blade.php
+++ resources/views/layouts/partials/nav/admin.blade.php
@@ -4,6 +4,7 @@
         @if($errorCount)<span class="badge badge-danger align-text-top">{{ $errorCount }}</span>@endif
     </a>
     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="{{ route('lans.index') }}">@lang('title.lans')</a>
         <a class="dropdown-item" href="{{ route('dashboard') }}" target="_blank">@lang('title.dashboard')</a>
         <a class="dropdown-item" href="{{ route('role-assignments.index') }}">@lang('title.role-assignments')</a>
         <a class="dropdown-item" href="{{ route('navigation-links.index') }}">@lang('title.navigation')</a>
