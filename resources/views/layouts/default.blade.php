6d409afb288a9a31fe2d8647f67df99ed8fcad79
ilumos <ilumos@gmail.com>
2019-02-23 22:45:52 +0000

    Remove ?fullscreen option

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,11 +1,4 @@
 @include('layouts.partials.header')
-@if( ! request()->has('fullscreen') )
-    @include('layouts.partials.nav')
-    @include('layouts.partials.content')
-    @include('layouts.partials.footer')
-@else
-    <main class="container-fullscreen">
-        @yield('content-header')
-        @yield('content')
-    </main>
-@endif
+@include('layouts.partials.nav')
+@include('layouts.partials.content')
+@include('layouts.partials.footer')
