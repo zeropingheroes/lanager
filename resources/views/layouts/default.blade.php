0ca83b227b7b72d0b80ff1b873646b78ea69f6b8
ilumos <ilumos@gmail.com>
2019-02-23 11:17:27 +0000

    Add ?fullscreen query string to hide any page's navbar

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,4 +1,11 @@
 @include('layouts.partials.header')
-@include('layouts.partials.nav')
-@include('layouts.partials.content')
-@include('layouts.partials.footer')
+@if( ! request()->has('fullscreen') )
+    @include('layouts.partials.nav')
+    @include('layouts.partials.content')
+    @include('layouts.partials.footer')
+@else
+    <main class="container-fullscreen">
+        @yield('content-header')
+        @yield('content')
+    </main>
+@endif
