5b62382e94167eb38d7e007c377b857afc591a5d
ilumos <ilumos@gmail.com>
2019-02-27 21:09:13 +0000

    Increase size of user avatar on live games page

--- resources/assets/js/components/users/user-avatar.vue
+++ resources/assets/js/components/users/user-avatar.vue
@@ -1,9 +1,25 @@
 <template>
-    <img :src="user.oauth_accounts[0].avatar.small" v-bind:title="user.username" />
+    <img :src="src" v-bind:title="user.username"/>
 </template>
 
 <script>
     export default {
-        props: ['user'],
+        props: {
+            size: {
+                type: String,
+                validator: function (value) {
+                    return ['small', 'medium', 'large'].indexOf(value) !== -1
+                },
+                default: 'medium',
+            },
+            user: {
+                type: Object,
+            },
+        },
+        computed: {
+            src() {
+                return this.user.oauth_accounts[0].avatar[this.size]
+            }
+        }
     }
 </script>
