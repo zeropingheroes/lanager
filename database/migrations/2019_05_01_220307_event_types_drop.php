57c147251ab91adcd55668cfbe88b5c0c85ed19c
ilumos <ilumos@gmail.com>
2019-05-02 22:10:46 +0100

    Remove event types

--- database/migrations/2019_05_01_220307_event_types_drop.php
+++ database/migrations/2019_05_01_220307_event_types_drop.php
@@ -13,6 +13,10 @@ class EventTypesDrop extends Migration
      */
     public function up()
     {
+        Schema::table('events', function (Blueprint $table) {
+            $table->dropForeign('events_event_type_id_foreign');
+            $table->dropColumn('event_type_id');
+        });
         Schema::drop('event_types');
     }
 
@@ -29,6 +33,17 @@ class EventTypesDrop extends Migration
             $table->string('colour')
                 ->nullable();
             $table->timestamps();
+        });
+
+        Schema::table('events', function (Blueprint $table) {
+            $table->integer('event_type_id')
+                ->unsigned()
+                ->nullable(); // Have to make it nullable or rollback fails due to events with no event type ID
+            $table->foreign('event_type_id')
+                ->references('id')
+                ->on('event_types')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
         });
     }
 }
