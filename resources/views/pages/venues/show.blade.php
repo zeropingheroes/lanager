8c809f4b10996557b6ae993b11037e5e112a099c
ilumos <ilumos@gmail.com>
2019-05-05 15:40:12 +0100

    Increase size of Google map embed

--- resources/views/pages/venues/show.blade.php
+++ resources/views/pages/venues/show.blade.php
@@ -26,7 +26,7 @@
     <a href="https://www.google.co.uk/maps?q={{$venue->street_address}}" target="_blank">{{$venue->street_address}}</a>
 
     <h4>@lang('title.map')</h4>
-    <iframe width="600" height="450" frameborder="0" style="border:0"
+    <iframe width="100%" height="500" frameborder="0" style="border:0"
             src="https://www.google.com/maps/embed/v1/search?q={{$venue->street_address}}&key={{ env('GOOGLE_API_KEY') }}"
             allowfullscreen></iframe>
 @endsection
