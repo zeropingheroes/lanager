7bd1c20ae54f32205690572318771ad5338ecdd2
ilumos <ilumos@gmail.com>
2019-06-23 13:14:38 +0100

    Rename listener to AwardLanAchievementToAttendee

--- app/Listeners/AwardLanAchievementToAttendee.php
+++ app/Listeners/AwardLanAchievementToAttendee.php
@@ -9,8 +9,9 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Zeropingheroes\Lanager\Lan;
 use Zeropingheroes\Lanager\UserAchievement;
 use Zeropingheroes\Lanager\WhitelistedIpRange;
+use Symfony\Component\HttpFoundation\IpUtils;
 
-class AddLanAchievement
+class AwardLanAchievementToAttendee
 {
     protected $request;
 
@@ -27,32 +28,40 @@ class AddLanAchievement
     /**
      * Handle the event.
      *
-     * @param  object  $event
+     * @param Login $login
      * @return void
      */
     public function handle(Login $login)
     {
         $lanHappeningNow = Lan::where('start', '<', now())
             ->where('end', '>', now())->first();
+
+        if(!$lanHappeningNow) {
+            return;
+        }
         $isAtLan = false;
 
         foreach(WhitelistedIpRange::pluck('ip_range') as $ipRange)
         {
-            if (\Symfony\Component\HttpFoundation\IpUtils::checkIp($this->request->ip(), $ipRange))
+            if (IpUtils::checkIp($this->request->ip(), $ipRange))
             {
                 $isAtLan = true;
                 break;
             }
         }
 
-        if($lanHappeningNow && $lanHappeningNow->has('achievement') && $isAtLan)
+        if($isAtLan && $lanHappeningNow && $lanHappeningNow->has('achievement'))
         {
             $userHasAchievement = $login->user->whereHas('achievements', function ($query) use ($lanHappeningNow) {
                 $query->where('id', $lanHappeningNow->achievement_id);
             })->first();
 
             if(!$userHasAchievement)
-                UserAchievement::create(['user_id' => $login->user->id, 'achievement_id' => $lanHappeningNow->achievement_id, 'lan_id' => $lanHappeningNow->id]);
+                UserAchievement::create([
+                    'user_id' => $login->user->id,
+                    'achievement_id' => $lanHappeningNow->achievement_id,
+                    'lan_id' => $lanHappeningNow->id
+                ]);
         }
     }
 }
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,7 +18,7 @@ class EventServiceProvider extends ServiceProvider
         ],
         'Illuminate\Auth\Events\Login' => [
             'Zeropingheroes\Lanager\Listeners\UpdateLanAttendeesTable',
-            'Zeropingheroes\Lanager\Listeners\AddLanAchievement',
+            'Zeropingheroes\Lanager\Listeners\AwardLanAchievementToAttendee',
             'Zeropingheroes\Lanager\Listeners\UpdateOutdatedUserAppsAfterSuccessfulAuth',
         ],
     ];
