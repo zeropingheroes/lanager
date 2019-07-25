ddf265fb9dc9a0bfeb06691b6334e46803adef5e
ilumos <ilumos@gmail.com>
2019-07-25 15:31:32 +0100

    Move LAN favourite game logic to service

--- app/Http/Controllers/LanFavouriteGameController.php
+++ app/Http/Controllers/LanFavouriteGameController.php
@@ -3,8 +3,8 @@
 namespace Zeropingheroes\Lanager\Http\Controllers;
 
 use Illuminate\Support\Facades\View;
-use Zeropingheroes\Lanager\Http\Resources\Game;
 use Zeropingheroes\Lanager\Lan;
+use Zeropingheroes\Lanager\Services\GetLanFavouriteGamesService;
 
 class LanFavouriteGameController extends Controller
 {
@@ -17,30 +17,11 @@ class LanFavouriteGameController extends Controller
      */
     public function index(Lan $lan)
     {
-        $favourites = $lan->userFavouriteGames()->with('user', 'favouriteable')->get();
+        $favourites = (new GetLanFavouriteGamesService($lan))->get();
 
-        if (empty($favourites)) {
-            return new Collection();
-        }
-
-        // Collect and combine favourites for the same game
-        $combined = [];
-        foreach ($favourites as $favourite) {
-            $combined[$favourite->favouriteable_id] = $combined[$favourite->favouriteable_id] ?? ['game' => null, 'users' => []];
-            $combined[$favourite->favouriteable_id]['game'] = $combined[$favourite->favouriteable_id]['game'] ?? $favourite->favouriteable;
-            $combined[$favourite->favouriteable_id]['users'][] = $favourite->user;
-        }
-
-        // Sort games array by user count, in descending order (removing key)
-        usort(
-            $combined,
-            function ($a, $b) {
-                return count($b['users']) - count($a['users']);
-            }
-        );
         return View::make('pages.lans.favourite-games.index')
             ->with('lan', $lan)
-            ->with('favourites', $combined);
+            ->with('favourites', $favourites);
     }
 
 }
---
+++ app/Services/GetLanFavouriteGamesService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Zeropingheroes\Lanager\Services;
+
+use Illuminate\Support\Collection;
+use Zeropingheroes\Lanager\Lan;
+
+class GetLanFavouriteGamesService
+{
+    /**
+     * @var Lan
+     */
+    private $lan;
+
+    public function __construct(Lan $lan)
+    {
+        $this->lan = $lan;
+    }
+
+    /**
+     * Get the games that are currently being played
+     *
+     * @return Collection
+     */
+    public function get(): Collection
+    {
+        $favourites = $this->lan->userFavouriteGames()->with('user', 'favouriteable')->get();
+
+        if (empty($favourites)) {
+            return new Collection();
+        }
+
+        // Collect and combine favourites for the same game
+        $combined = [];
+        foreach ($favourites as $favourite) {
+            $combined[$favourite->favouriteable_id] = $combined[$favourite->favouriteable_id] ?? ['game' => null, 'users' => []];
+            $combined[$favourite->favouriteable_id]['game'] = $combined[$favourite->favouriteable_id]['game'] ?? $favourite->favouriteable;
+            $combined[$favourite->favouriteable_id]['users'][] = $favourite->user;
+        }
+
+        // Sort games array by user count, in descending order (removing key)
+        usort(
+            $combined,
+            function ($a, $b) {
+                return count($b['users']) - count($a['users']);
+            }
+        );
+        return collect($combined);
+    }
+}
