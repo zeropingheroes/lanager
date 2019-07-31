7433cbf7b3af84c6a85c1991180961b0fb26af10
ilumos <ilumos@gmail.com>
2019-07-31 21:39:13 +0100

    Display error(s) when picking a game

--- resources/views/pages/lans/attendee-game-picks/index.blade.php
+++ resources/views/pages/lans/attendee-game-picks/index.blade.php
@@ -13,28 +13,35 @@
         <p class="mt-2">@lang('phrase.lan-attendee-game-picks-user-help')</p>
         <script>
             window.addEventListener('load', function() {
-                window.lanId = {{ $lan->id }}
                 const app = new Vue({
                     el: '#app',
-                    // methods: {
-                    //     post(game) {
-                    //         game.lanId = lanId
-                    //         axios.post('lans/' + lanId + '/attendee-game-picks', game)
-                    //             .then((response) => {
-                    //                 location.reload();
-                    //             }, (error) => {
-                    //                 console.log('Error adding game')
-                    //                 console.log(error.response.status)
-                    //                 console.log(error.response.data)
-                    //             })
-                    //     }
-                    // }
+                    methods: {
+                        post(game) {
+                            console.log(game);
+                            pick = {
+                                game_provider: game.provider,
+                                game_id: game.id,
+                            }
+                            axios.post('lans/' + {{ $lan->id }} + '/attendee-game-picks', pick)
+                                .then((response) => {
+                                    location.reload();
+                                }, (error) => {
+                                    error.response.data.errors.forEach(function(error) {
+                                        $("#alerts").append('<div class="alert alert-danger" role="alert">' +
+                                            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+                                            '<span aria-hidden="true">&times;</span>' +
+                                            '</button><span>'+error+'</span></div>')
+                                    });
+                                })
+                        }
+                    }
                 });
             });
         </script>
-        <div id="app">
+        <div id="app" class="mb-3">
             <games-search @selected="post"></games-search>
         </div>
+        <div id="alerts"></div>
     @else
         <p class="mt-2">@lang('phrase.lan-attendee-game-picks-guest-help')</p>
     @endif
