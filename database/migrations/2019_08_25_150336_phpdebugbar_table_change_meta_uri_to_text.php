2ef6b696d201bb8e9a182880bfda6f93d86bfabc
ilumos <ilumos@gmail.com>
2019-08-25 15:24:12 +0100

    Fix truncated meta_uri in phpdebugbar

---
+++ database/migrations/2019_08_25_150336_phpdebugbar_table_change_meta_uri_to_text.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PhpdebugbarTableChangeMetaUriToText extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('phpdebugbar', function (Blueprint $table) {
+            $table->renameColumn('meta_uri', 'meta_uri_old');
+        });
+        Schema::table('phpdebugbar', function (Blueprint $table) {
+            $table->text('meta_uri');
+        });
+
+        // Copy existing data over
+        DB::table('phpdebugbar')
+            ->update([
+                'meta_uri' => DB::raw('`meta_uri_old`'),
+            ]);
+
+        // Drop old column
+        Schema::table('phpdebugbar', function (Blueprint $table) {
+            $table->dropColumn('meta_uri_old');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('phpdebugbar', function (Blueprint $table) {
+            $table->renameColumn('meta_uri', 'meta_uri_old');
+        });
+        Schema::table('phpdebugbar', function (Blueprint $table) {
+            $table->text('meta_uri');
+        });
+
+        // Copy existing data over
+        DB::table('phpdebugbar')
+            ->update([
+                'meta_uri' => DB::raw('`meta_uri_old`'),
+            ]);
+
+        // Drop old column
+        Schema::table('phpdebugbar', function (Blueprint $table) {
+            $table->dropColumn('meta_uri_old');
+        });
+    }
+}
