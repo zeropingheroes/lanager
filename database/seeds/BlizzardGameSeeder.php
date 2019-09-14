65b408126132503b01e233fa19b6f44f4a7bfcae
ilumos <ilumos@gmail.com>
2019-09-14 18:19:55 +0100

    Allow seeder to create/upate/delete Blizzard games

--- database/seeds/BlizzardGameSeeder.php
+++ database/seeds/BlizzardGameSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Zeropingheroes\Lanager\BlizzardGame;
+use Zeropingheroes\Lanager\LanAttendeeGamePick;
 
 class BlizzardGameSeeder extends Seeder
 {
@@ -12,71 +13,69 @@ class BlizzardGameSeeder extends Seeder
      */
     public function run()
     {
-        // don't seed if table is not empty
-        if (BlizzardGame::count()) {
-            return;
-        }
-
         $games = [
-            [
-                'id' => 1,
+            1 => [
                 'name' => 'Call of Duty: Black Ops 4',
                 'url' => 'https://shop.battle.net/product/call-of-duty-black-ops-4'
             ],
-            [
-                'id' => 2,
+            2 => [
                 'name' => 'Call of Duty: Modern Warfare',
                 'url' => 'https://shop.battle.net/family/call-of-duty-mw'
             ],
-            [
-                'id' => 5,
+            5 => [
                 'name' => 'Diablo II',
                 'url' => 'https://www.blizzard.com/games/d2/'
             ],
-            [
-                'id' => 6,
+            6 => [
                 'name' => 'Diablo III',
                 'url' => 'https://shop.battle.net/family/diablo-iii'
             ],
-            [
-                'id' => 7,
+            7 => [
                 'name' => 'Hearthstone',
                 'url' => 'https://shop.battle.net/family/hearthstone'
             ],
-            [
-                'id' => 8,
+            8 => [
                 'name' => 'Heroes of the Storm',
                 'url' => 'https://shop.battle.net/family/heroes-of-the-storm'
             ],
-            [
-                'id' => 9,
+            9 => [
                 'name' => 'Overwatch',
                 'url' => 'https://shop.battle.net/family/overwatch'
             ],
-            [
-                'id' => 10,
+            10 => [
                 'name' => 'StarCraft',
                 'url' => 'https://shop.battle.net/product/starcraft'
             ],
-            [
-                'id' => 11,
+            11 => [
                 'name' => 'StarCraft II: Wings of Liberty',
                 'url' => 'https://shop.battle.net/family/starcraft-ii'
             ],
-            [
-                'id' => 14,
+            14 => [
                 'name' => 'Warcraft III: Reign of Chaos',
                 'url' => 'https://www.blizzard.com/en-gb/games/war3/'
             ],
-            [
-                'id' => 15,
+            15 => [
                 'name' => 'World of Warcraft',
                 'url' => 'https://shop.battle.net/family/world-of-warcraft'
             ],
         ];
 
-        foreach($games as $game) {
-            BlizzardGame::create($game);
+        foreach ($games as $gameId => $game) {
+            BlizzardGame::updateOrCreate(['id' => $gameId], $game);
+        }
+
+        // Delete games and game picks from the database that don't exist in the seeder
+        $databaseIds = BlizzardGame::all()->pluck('id')->toArray();
+        foreach ($databaseIds as $databaseId) {
+            if (!array_key_exists($databaseId, $games)) {
+                LanAttendeeGamePick::where(
+                    [
+                        ['game_id_type', '=', 'blizzard'],
+                        ['game_id', '=', $databaseId]
+                    ]
+                )->delete();
+                BlizzardGame::destroy($databaseId);
+            }
         }
     }
 }
