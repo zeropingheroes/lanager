08ec0190214c293d6f0bf083a77051b0ffccbdb6
ilumos <ilumos@gmail.com>
2020-01-18 16:39:31 +0000

    Allow crawling but disallow indexing and link following

--- resources/views/layouts/partials/html-start.blade.php
+++ resources/views/layouts/partials/html-start.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robots" content="noindex, nofollow">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="api-base-url" content="{{ url('/api') }}">
