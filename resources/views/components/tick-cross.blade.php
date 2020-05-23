49f6d88a294fb1c888adf2ae3de28fc151b914f6
ilumos <ilumos@gmail.com>
2020-05-23 20:31:06 +0100

    Fix incorrect titles

--- resources/views/components/tick-cross.blade.php
+++ resources/views/components/tick-cross.blade.php
@@ -1,7 +1,7 @@
 @if($value)
-    <span class="oi oi-check" title="Cog" aria-hidden="true"></span>
+    <span class="oi oi-check" title="Yes" aria-hidden="true"></span>
 @else
     @if(isset($showCross))
-    <span class="oi oi-x" title="Cog" aria-hidden="true"></span>
+    <span class="oi oi-x" title="No" aria-hidden="true"></span>
     @endif
 @endif
