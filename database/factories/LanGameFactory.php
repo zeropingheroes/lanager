7bcd220cf987ac612d2c2de01fd5ad890b1bfcc2
ilumos <ilumos@gmail.com>
2020-06-07 20:00:17 +0100

    Use random Steam games for LAN game test data

--- database/factories/LanGameFactory.php
+++ database/factories/LanGameFactory.php
@@ -5,12 +5,13 @@
 use Faker\Generator as Faker;
 use Zeropingheroes\Lanager\Lan;
 use Zeropingheroes\Lanager\LanGame;
+use Zeropingheroes\Lanager\SteamApp;
 use Zeropingheroes\Lanager\User;
 
 $factory->define(LanGame::class, function (Faker $faker) {
     return [
         'lan_id' => Lan::all()->random()->id,
-        'game_name' => $faker->word,
+        'game_name' => SteamApp::where('type','=','game')->inRandomOrder()->first()->name,
         'created_by' => User::all()->random()->id,
     ];
 });
