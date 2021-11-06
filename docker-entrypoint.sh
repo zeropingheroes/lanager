cb7f3a2e7c688dd481b0d833d531627922ff55ae
ilumos <ilumos@gmail.com>
2021-11-06 12:20:01 +0000

    Do not prompt for input during installation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,9 +11,8 @@ FILE="/var/www/storage/.install-completed"
 # Check if installation has been completed
 if ! [[ -f $FILE ]]; then
 
-    php artisan migrate:fresh
-    php artisan db:seed --class=Database\\Seeders\\DatabaseSeeder
-    php artisan storage:link
+    php artisan migrate:fresh --force
+    php artisan db:seed --force --class=Database\\Seeders\\DatabaseSeeder
     php artisan lanager:import-steam-apps-csv
 
     touch $FILE
