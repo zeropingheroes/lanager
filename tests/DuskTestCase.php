01fbb7b7b75b348ff74b57acc76583f7d6e47357
ilumos <ilumos@gmail.com>
2022-09-19 15:11:02 +0100

    Install Laravel Dusk for browser testing

---
+++ tests/DuskTestCase.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Tests;
+
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Laravel\Dusk\Browser;
+use Laravel\Dusk\TestCase as BaseTestCase;
+
+abstract class DuskTestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Browser::$storeScreenshotsAt = storage_path('logs/dusk/screenshots');
+        Browser::$storeConsoleLogAt = storage_path('logs/dusk/console');
+    }
+
+    /**
+     * Prepare for Dusk test execution.
+     *
+     * @beforeClass
+     * @return void
+     */
+    public static function prepare()
+    {
+        // if (! static::runningInSail()) {
+        //     static::startChromeDriver();
+        // }
+    }
+
+    /**
+     * Create the RemoteWebDriver instance.
+     *
+     * @return \Facebook\WebDriver\Remote\RemoteWebDriver
+     */
+    protected function driver()
+    {
+        $options = (new ChromeOptions())->addArguments(
+            collect([
+                $this->shouldStartMaximized() ? '--start-maximized' : '--window-size=1920,1080',
+            ])->unless($this->hasHeadlessDisabled(), function ($items) {
+                return $items->merge([
+                    '--disable-gpu',
+                    '--headless',
+                    '--remote-debugging-port=9222'
+                ]);
+            })->all()
+        );
+
+        return RemoteWebDriver::create(
+            env('DUSK_DRIVER_URL'),
+            DesiredCapabilities::chrome()->setCapability(
+                ChromeOptions::CAPABILITY,
+                $options
+            )
+        );
+    }
+
+    /**
+     * Determine whether the Dusk command has disabled headless mode.
+     *
+     * @return bool
+     */
+    protected function hasHeadlessDisabled()
+    {
+        return isset($_SERVER['DUSK_HEADLESS_DISABLED']) ||
+            isset($_ENV['DUSK_HEADLESS_DISABLED']);
+    }
+
+    /**
+     * Determine if the browser window should start maximized.
+     *
+     * @return bool
+     */
+    protected function shouldStartMaximized()
+    {
+        return isset($_SERVER['DUSK_START_MAXIMIZED']) ||
+            isset($_ENV['DUSK_START_MAXIMIZED']);
+    }
+}
