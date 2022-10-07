91f600728b15674c40e11e72cfd1b8a7b87261e9
ilumos <ilumos@gmail.com>
2022-10-07 20:54:23 +0100

    Move dropdown title from span to button

--- resources/views/components/actions-dropdown.blade.php
+++ resources/views/components/actions-dropdown.blade.php
@@ -1,6 +1,11 @@
 <div class="btn-group">
-    <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <span class="oi oi-cog" title="Cog" aria-hidden="true"></span>
+    <button class="btn btn-secondary btn-sm dropdown-toggle"
+            type="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+            title="Options">
+        <span class="oi oi-cog" aria-hidden="true"></span>
     </button>
     <div class="dropdown-menu">
         {{ $slot }}
