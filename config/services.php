de987e1f8136b618722dbbd1f162dd1b8991cd8b
ilumos <ilumos@gmail.com>
2024-07-30 16:45:34 +0100

    Remove unused services from `services.php`

--- config/services.php
+++ config/services.php
@@ -14,23 +14,6 @@ return [
     |
     */
 
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-        'scheme' => 'https',
-    ],
-
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
-    ],
-
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
-
     'steam' => [
         'client_id' => null,
         'client_secret' => env('STEAM_API_KEY'),
