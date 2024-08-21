30a5bb16897cc46e2e2917ba0678ef0f212bcfac
ilumos <ilumos@gmail.com>
2024-08-21 16:13:49 +0100

    Add `TRUSTED_PROXIES` to fix mixed content error with reverse proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(at: explode(',', env('TRUSTED_PROXIES', '')));
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
