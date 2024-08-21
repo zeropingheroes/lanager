a4c33c5f2016b25c4060745b977891dd31d4dc19
ilumos <ilumos@gmail.com>
2024-08-21 18:02:51 +0100

    Load `AppServiceProvider`

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    GrahamCampbell\Markdown\MarkdownServiceProvider::class,
-    Zeropingheroes\Lanager\Providers\ViewComposerServiceProvider::class,
+    Zeropingheroes\Lanager\Providers\AppServiceProvider::class,
     Zeropingheroes\Lanager\Providers\AuthServiceProvider::class,
+    Zeropingheroes\Lanager\Providers\ViewComposerServiceProvider::class,
+    GrahamCampbell\Markdown\MarkdownServiceProvider::class,
 ];
