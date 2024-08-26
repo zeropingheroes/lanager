09beb1817df0063206fd166508c4fb2cac406864
ilumos <ilumos@gmail.com>
2024-08-26 21:33:10 +0100

    Set up Vite and Bootstrap 5.3

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,71 +1,10 @@
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-import langs from './langs.js';
-import Vue from 'vue'
+import axios from 'axios';
 
-try {
-    window.$ = window.jQuery = require('jquery');
-    window.moment = require('moment');
-    window.Clipboard = require('clipboard');
-    window.Vue = Vue;
-    window.validator = require('validator');
-    var Lang = require('lang.js');
-    window.lang = new Lang({messages: langs});
-    require('tempusdominus-bootstrap-4');
-    require('bootstrap');
-} catch (e) {
-    console.error(e);
-}
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = require('axios');
-import FullCalendar from 'vue-full-calendar';
-Vue.use(FullCalendar);
+window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.baseURL = document.head.querySelector('meta[name="api-base-url"]').content;
 
-if(document.head.querySelector("meta[name=api-user-id]")) {
-    window.axios.defaults.headers.common['Authorization'] = 'Bearer ' + document.head.querySelector("meta[name=api-token]").content;
-    window.userId = Number(document.head.querySelector("meta[name=api-user-id]").content);
-}
+import * as Popper from '@popperjs/core'
 
-
-/**
- * Next we will register the CSRF Token as a common header with Axios so that
- * all outgoing HTTP requests automatically have it attached. This is just
- * a simple convenience so we don't have to attach every token manually.
- */
-
-let token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo';
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
+window.Popper = Popper
+import 'bootstrap'
