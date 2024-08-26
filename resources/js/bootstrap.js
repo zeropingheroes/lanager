3943f8602c2bafbb30c063abc855935b05485a3e
ilumos <ilumos@gmail.com>
2024-08-26 23:24:49 +0100

    Group related lines together

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
-
 window.axios = axios;
-
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import * as Popper from '@popperjs/core'
 
-window.Popper = Popper
+window.Popper = Popper;
+
 import 'bootstrap'
 
 import {TempusDominus} from '@eonasdan/tempus-dominus';
