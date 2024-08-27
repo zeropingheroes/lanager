629f339298e136e5857e57e4c79a13b5bb5be6b1
ilumos <ilumos@gmail.com>
2024-08-27 22:46:19 +0100

    Add `vue` alias to Vite config

--- vite.config.js
+++ vite.config.js
@@ -30,6 +30,11 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
     server: {
         hmr: {
             host: 'localhost',
