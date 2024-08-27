5670e09fe013c2007caf00ac414935820525900a
ilumos <ilumos@gmail.com>
2024-08-27 20:55:05 +0100

    Group related lines together

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,11 +3,9 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import * as Popper from '@popperjs/core'
-
 window.Popper = Popper;
 
 import 'bootstrap'
 
 import {TempusDominus} from '@eonasdan/tempus-dominus';
-
 window.TempusDominus = TempusDominus;
