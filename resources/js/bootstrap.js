d68980567f68b5acbc3913b12b6b4b9c2381902e
ilumos <ilumos@gmail.com>
2024-08-30 20:18:37 +0100

    Add base URL for axios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.baseURL = document.head.querySelector('meta[name="api-base-url"]').content;
 
 import * as Popper from '@popperjs/core'
 window.Popper = Popper;
