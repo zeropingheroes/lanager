87242e988733d416a2fb822e6fd2ca4c15a0c531
ilumos <ilumos@gmail.com>
2024-09-05 22:50:57 +0100

    Update component for single slide preview to Vue 3 composition API

--- resources/js/pages/slides.js
+++ resources/js/pages/slides.js
@@ -1,7 +1,9 @@
 import Slides from '../components/slides/slides.vue'
+import SlidesSingle from '../components/slides/slides-single.vue'
 import FullscreenButton from '../components/fullscreen-button.vue'
 
 const app = createApp({});
 app.component('Slides', Slides);
+app.component('SlidesSingle', SlidesSingle);
 app.component('FullscreenButton', FullscreenButton);
 app.mount('#app');
