5701459599d66539eeffbd78812703bd68cb0ced
ilumos <ilumos@gmail.com>
2024-09-15 17:00:02 +0100

    Fix deletion confirmation on button partial

--- resources/views/components/buttons/delete.blade.php
+++ resources/views/components/buttons/delete.blade.php
@@ -2,9 +2,9 @@
     @php
         $route = \Illuminate\Support\Str::kebab(\Illuminate\Support\Str::plural(class_basename($item)));
     @endphp
-    <form action="{{ route( $route . '.destroy', $item->id) }}" method="POST" class="d-inline confirm-deletion">
+    <form action="{{ route( $route . '.destroy', $item->id) }}" method="POST" class="d-inline">
         {{ method_field('DELETE') }}
         {{ csrf_field() }}
-        <button type="submit" class="btn btn-danger">@lang('title.delete')</button>
+        <button type="submit" class="btn btn-danger" onclick="submitDeletionForm(event)">@lang('title.delete')</button>
     </form>
 @endcan
