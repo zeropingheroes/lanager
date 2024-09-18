e8621f872dd362f46185b84a2a2deaa69510e56b
ilumos <ilumos@gmail.com>
2024-09-18 22:01:13 +0100

    Remove profile status from attendee list

--- resources/views/pages/users/partials/list.blade.php
+++ resources/views/pages/users/partials/list.blade.php
@@ -2,7 +2,6 @@
     <thead>
     <tr>
         <th>@lang('title.username')</th>
-        <th>@lang('title.profile')</th>
         <th colspan="3">@lang('title.status')</th>
     </tr>
     </thead>
@@ -11,9 +10,6 @@
         <tr>
             <td>
                 @include('pages.users.partials.avatar-username', ['user' => $user])
-            </td>
-            <td>
-                @include('pages.users.partials.private-profile-badge', ['user' => $user])
             </td>
             <td>
                 @include('pages.users.partials.online-status-badge', ['user' => $user])
