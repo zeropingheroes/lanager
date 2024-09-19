4859ab04cbd5e4d1d0a82f387f0a355be7bbcb6d
ilumos <ilumos@gmail.com>
2024-09-19 20:02:59 +0100

    Remove unused 'description' column from LANs table

---
+++ database/migrations/2024_09_19_195711_remove_lan_description.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveLanDescription extends Migration
+{
+    /**
+     * Run the migrations.
+     * @throws Exception
+     */
+    public function up(): void
+    {
+        Schema::table('lans', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     */
+    public function down(): void
+    {
+        Schema::table('lans', function (Blueprint $table) {
+            $table->addColumn('description', 'text')->nullable();
+        });
+    }
+}
