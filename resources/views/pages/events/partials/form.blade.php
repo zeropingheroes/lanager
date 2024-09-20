6e0d0f7cb6ea9f9b8704c947018c8b540278bc6d
ilumos <ilumos@gmail.com>
2024-09-20 14:20:46 +0100

    Move fields into reusable components

--- resources/views/pages/events/partials/form.blade.php
+++ resources/views/pages/events/partials/form.blade.php
@@ -1,84 +1,7 @@
-<div class="row mb-3">
-    <label for="name"
-           class="col-sm-2 col-form-label"
-    >
-        @lang('title.name')
-    </label>
-    <div class="col-sm-10">
-        <input type="text"
-               class="form-control"
-               id="name"
-               name="name"
-               placeholder="@lang('title.name')"
-               value="{{ old('name', $event->name) }}"
-        >
-    </div>
-</div>
-<div class="row mb-3">
-    <label for="description"
-           class="col-sm-2 col-form-label"
-    >
-        @lang('title.description')
-    </label>
-    <div class="col-sm-10">
-        <textarea class="form-control"
-                  id="description"
-                  name="description"
-                  rows="10"
-                  placeholder="@lang('phrase.markdown-help')"
-                  aria-describedby="descriptionHelp"
-        >{{ old('description', $event->description) }}</textarea>
-        <small id="descriptionHelp"
-               class="form-text text-muted"
-        >
-            <a href="@lang('phrase.markdown-formatting-help-link-url')"
-               target="_blank"
-            >
-                @lang('phrase.markdown-formatting-help-link')
-            </a>
-            <br>
-            <a href="{{ route('images.index') }}"
-               target="_blank"
-            >
-                @lang('title.upload-images')
-            </a>
-        </small>
-    </div>
-</div>
-<div class="row mb-3">
-    <label for="start"
-           class="col-sm-2 col-form-label"
-    >
-        @lang('title.start')
-    </label>
-    <div class="col-sm-4">
-        <input type="text"
-               class="form-control datetimepicker-input"
-               id="start"
-               name="start"
-               placeholder="YYYY-MM-DD HH:MM:SS"
-               value="{{ old('start', $event->start) }}"
-               data-toggle="datetimepicker"
-               data-target="#start"
-        >
-    </div>
-    <label for="end"
-           class="col-sm-2 col-form-label"
-    >
-        @lang('title.end')
-    </label>
-    <div class="col-sm-4">
-        <input type="text"
-               class="form-control datetimepicker-input"
-               id="end"
-               name="end"
-               placeholder="YYYY-MM-DD HH:MM:SS"
-               value="{{ old('end', $event->end) }}"
-               data-toggle="datetimepicker"
-               data-target="#end"
-        >
-    </div>
-</div>
+@include('components.form.inputs.name', ['value' => $event->name])
+@include('components.form.inputs.description', ['value' => $event->description])
+@include('components.form.inputs.start-end', ['start' => $event->start, 'end' => $event->end])
+
 <div class="row mb-3">
     <label for="signups_open"
            class="col-sm-2 col-form-label"
@@ -114,29 +37,9 @@
     </div>
 </div>
 
-<div class="row mb-3">
-    <label for="published"
-           class="col-sm-2 col-form-label"
-    >
-        @lang('title.published')
-    </label>
-    <div class="col-sm-10">
-        <div class="form-check">
-            <input type="checkbox"
-                   class="form-check-input"
-                   id="published"
-                   name="published"
-                   value="1"
-                {{ old('published', $event->published) ? 'checked' : null}}
-            >
-        </div>
-    </div>
-</div>
-<div class="row mb-3">
-    <div class="offset-sm-2 d-grid col-sm-10 gap-2">
-        <button type="submit" class="btn btn-primary">@lang('title.submit')</button>
-    </div>
-</div>
+@include('components.form.inputs.published', ['value' => $event->published])
+@include('components.form.inputs.submit')
+
 <script>
     document.addEventListener("DOMContentLoaded", function () {
 
