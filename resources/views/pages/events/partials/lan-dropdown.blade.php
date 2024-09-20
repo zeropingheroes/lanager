fa6a0b17a3bdb939fcf8144312bbbea4758422b1
ilumos <ilumos@gmail.com>
2024-09-20 15:26:02 +0100

    Remove unused view partial

--- resources/views/pages/events/partials/lan-dropdown.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="form-group mb-3">
-    <label for="lan_id">@lang('title.lan')</label>
-    @include('components.form.select', ['name' => 'lan_id', 'item' => $event, 'items' => $lans, 'labelField' => 'name'])
-</div>
