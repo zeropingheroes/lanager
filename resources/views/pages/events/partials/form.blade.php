105f1837f8d706d871dd7a861402b73251f8d299
ilumos <ilumos@gmail.com>
2024-09-20 12:45:22 +0100

    Change event form to horizontal

--- resources/views/pages/events/partials/form.blade.php
+++ resources/views/pages/events/partials/form.blade.php
@@ -1,55 +1,140 @@
-<div class="form-group mb-3">
-    <label for="name">@lang('title.name')</label>
-    <input type="text" class="form-control" id="name" name="name" placeholder="@lang('title.name')"
-           value="{{ old('name', $event->name) }}">
-</div>
-<div class="form-group mb-3">
-    <label for="description">@lang('title.description')</label>
-    <textarea class="form-control" id="description" name="description" rows="10" placeholder="@lang('phrase.markdown-help')"
-              aria-describedby="descriptionHelp">{{ old('description', $event->description) }}</textarea>
-    <small id="descriptionHelp" class="form-text text-muted">
-        <a href="@lang('phrase.markdown-formatting-help-link-url')" target="_blank">@lang('phrase.markdown-formatting-help-link')</a>
-        <br>
-        <a href="{{ route('images.index') }}" target="_blank">@lang('title.upload-images')</a>
-    </small>
-</div>
-<div class="form-row">
-    <div class="form-group col-md-6">
-        <label for="start">@lang('title.start')</label>
-        <input type="text" class="form-control datetimepicker-input" id="start" name="start"
-               placeholder="YYYY-MM-DD HH:MM:SS" value="{{ old('start', $event->start) }}"
-               data-toggle="datetimepicker" data-target="#start">
-    </div>
-
-    <div class="form-group col-md-6">
-        <label for="end">@lang('title.end')</label>
-        <input type="text" class="form-control datetimepicker-input" id="end" name="end"
-               placeholder="YYYY-MM-DD HH:MM:SS" value="{{ old('end', $event->end) }}"
-               data-toggle="datetimepicker" data-target="#end">
+<div class="row mb-3">
+    <label for="name"
+           class="col-sm-2 col-form-label"
+    >
+        @lang('title.name')
+    </label>
+    <div class="col-sm-10">
+        <input type="text"
+               class="form-control"
+               id="name"
+               name="name"
+               placeholder="@lang('title.name')"
+               value="{{ old('name', $event->name) }}"
+        >
     </div>
 </div>
-
-<div class="form-row">
-    <div class="form-group col-md-6">
-        <label for="signups_open">@lang('title.signups-open')</label>
-        <input type="text" class="form-control datetimepicker-input" id="signups_open" name="signups_open"
-               placeholder="YYYY-MM-DD HH:MM:SS" value="{{ old('signups_open', $event->signups_open) }}"
-               data-toggle="datetimepicker" data-target="#signups_open">
+<div class="row mb-3">
+    <label for="description"
+           class="col-sm-2 col-form-label"
+    >
+        @lang('title.description')
+    </label>
+    <div class="col-sm-10">
+        <textarea class="form-control"
+                  id="description"
+                  name="description"
+                  rows="10"
+                  placeholder="@lang('phrase.markdown-help')"
+                  aria-describedby="descriptionHelp"
+        >{{ old('description', $event->description) }}</textarea>
+        <small id="descriptionHelp"
+               class="form-text text-muted"
+        >
+            <a href="@lang('phrase.markdown-formatting-help-link-url')"
+               target="_blank"
+            >
+                @lang('phrase.markdown-formatting-help-link')
+            </a>
+            <br>
+            <a href="{{ route('images.index') }}"
+               target="_blank"
+            >
+                @lang('title.upload-images')
+            </a>
+        </small>
     </div>
-
-    <div class="form-group col-md-6">
-        <label for="signups_close">@lang('title.signups-close')</label>
-        <input type="text" class="form-control datetimepicker-input" id="signups_close" name="signups_close"
-               placeholder="YYYY-MM-DD HH:MM:SS" value="{{ old('signups_close', $event->signups_close) }}"
-               data-toggle="datetimepicker" data-target="#signups_close">
+</div>
+<div class="row mb-3">
+    <label for="start"
+           class="col-sm-2 col-form-label"
+    >
+        @lang('title.start')
+    </label>
+    <div class="col-sm-10">
+        <input type="text"
+               class="form-control datetimepicker-input"
+               id="start"
+               name="start"
+               placeholder="YYYY-MM-DD HH:MM:SS"
+               value="{{ old('start', $event->start) }}"
+               data-toggle="datetimepicker"
+               data-target="#start"
+        >
+    </div>
+</div>
+<div class="row mb-3">
+    <label for="end"
+           class="col-sm-2 col-form-label"
+    >
+        @lang('title.end')
+    </label>
+    <div class="col-sm-10">
+        <input type="text"
+               class="form-control datetimepicker-input"
+               id="end"
+               name="end"
+               placeholder="YYYY-MM-DD HH:MM:SS"
+               value="{{ old('end', $event->end) }}"
+               data-toggle="datetimepicker"
+               data-target="#end"
+        >
     </div>
 </div>
 
-<div class="form-group mb-3">
-    <div class="custom-control custom-checkbox">
-        <input type="checkbox" class="custom-control-input" id="published" name="published"
-               value="1" {{ old('published', $event->published) ? 'checked' : null}}>
-        <label class="custom-control-label" for="published">@lang('title.published')</label>
+<div class="row mb-3">
+    <label for="signups_open"
+           class="col-sm-2 col-form-label"
+    >
+        @lang('title.signups-open')
+    </label>
+    <div class="col-sm-10">
+        <input type="text"
+               class="form-control datetimepicker-input"
+               id="signups_open"
+               name="signups_open"
+               placeholder="YYYY-MM-DD HH:MM:SS"
+               value="{{ old('signups_open', $event->signups_open) }}"
+               data-toggle="datetimepicker"
+               data-target="#signups_open"
+        >
+    </div>
+</div>
+<div class="row mb-3">
+    <label for="signups_close"
+           class="col-sm-2 col-form-label"
+    >
+        @lang('title.signups-close')
+    </label>
+    <div class="col-sm-10">
+        <input type="text"
+               class="form-control datetimepicker-input"
+               id="signups_close"
+               name="signups_close"
+               placeholder="YYYY-MM-DD HH:MM:SS"
+               value="{{ old('signups_close', $event->signups_close) }}"
+               data-toggle="datetimepicker"
+               data-target="#signups_close"
+        >
+    </div>
+</div>
+
+<div class="row mb-3">
+    <div class="col-sm-10 offset-sm-2">
+        <div class="form-check">
+            <input type="checkbox"
+                   class="form-check-input"
+                   id="published"
+                   name="published"
+                   value="1"
+                {{ old('published', $event->published) ? 'checked' : null}}
+            >
+            <label class="form-check-label"
+                   for="published"
+            >
+                @lang('title.published')
+            </label>
+        </div>
     </div>
 </div>
 <button type="submit" class="btn btn-primary">@lang('title.submit')</button>
