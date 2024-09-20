6b07b5c6b592a2eeba5ec972268686d231d73ef0
ilumos <ilumos@gmail.com>
2024-09-20 13:03:21 +0100

    Move script to end of component

--- resources/views/pages/lans/partials/form.blade.php
+++ resources/views/pages/lans/partials/form.blade.php
@@ -4,56 +4,6 @@
            value="{{ old('name', $lan->name) }}">
 </div>
 <div class="form-row mb-3">
-    <script>
-        document.addEventListener("DOMContentLoaded", function () {
-
-            const startInput = document.getElementById('start');
-            const start = new TempusDominus(startInput, {
-                localization: {
-                    format: 'yyyy-MM-dd HH:mm',
-                    dayViewHeaderFormat: {month: 'long', year: 'numeric'},
-                },
-                display: {
-                    sideBySide: true,
-                    theme: "dark",
-                    buttons: {
-                        today: true,
-                        clear: true,
-                    },
-                },
-            });
-            if (startInput.value) {
-                start.dates.setValue(start.dates.parseInput(startInput.value));
-            }
-
-            const endInput = document.getElementById('end');
-            const end = new TempusDominus(endInput, {
-                localization: {
-                    format: 'yyyy-MM-dd HH:mm',
-                    dayViewHeaderFormat: {month: 'long', year: 'numeric'},
-                },
-                display: {
-                    sideBySide: true,
-                    theme: "dark",
-                    buttons: {
-                        today: true,
-                        clear: true,
-                    },
-                },
-            });
-            if (endInput.value) {
-                end.dates.setValue(end.dates.parseInput(endInput.value));
-            }
-
-            startInput.addEventListener(Namespace.events.change, (e) => {
-                end.updateOptions({
-                    restrictions: {
-                        minDate: e.detail.date,
-                    },
-                });
-            });
-        });
-    </script>
     <div class="form-group col-md-6 mb-3">
         <label for="start">@lang('title.start')</label>
         <input type="text" class="form-control datetimepicker-input" id="start" name="start"
@@ -88,3 +38,53 @@
     </div>
 </div>
 <button type="submit" class="btn btn-primary">@lang('title.submit')</button>
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+
+        const startInput = document.getElementById('start');
+        const start = new TempusDominus(startInput, {
+            localization: {
+                format: 'yyyy-MM-dd HH:mm',
+                dayViewHeaderFormat: {month: 'long', year: 'numeric'},
+            },
+            display: {
+                sideBySide: true,
+                theme: "dark",
+                buttons: {
+                    today: true,
+                    clear: true,
+                },
+            },
+        });
+        if (startInput.value) {
+            start.dates.setValue(start.dates.parseInput(startInput.value));
+        }
+
+        const endInput = document.getElementById('end');
+        const end = new TempusDominus(endInput, {
+            localization: {
+                format: 'yyyy-MM-dd HH:mm',
+                dayViewHeaderFormat: {month: 'long', year: 'numeric'},
+            },
+            display: {
+                sideBySide: true,
+                theme: "dark",
+                buttons: {
+                    today: true,
+                    clear: true,
+                },
+            },
+        });
+        if (endInput.value) {
+            end.dates.setValue(end.dates.parseInput(endInput.value));
+        }
+
+        startInput.addEventListener(Namespace.events.change, (e) => {
+            end.updateOptions({
+                restrictions: {
+                    minDate: e.detail.date,
+                },
+            });
+        });
+    });
+</script>
