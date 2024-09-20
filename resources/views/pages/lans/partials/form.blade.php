043c1aa6c9cbfb8fb5dfbc7a60e31960d00a85df
ilumos <ilumos@gmail.com>
2024-09-20 14:34:13 +0100

    Use reusable components in LAN form

--- resources/views/pages/lans/partials/form.blade.php
+++ resources/views/pages/lans/partials/form.blade.php
@@ -1,54 +1,5 @@
-<div class="row mb-3">
-    <label for="name"
-           class="col-sm-2 col-form-label"
-    >
-        @lang('title.name')
-    </label>
-    <div class="col-sm-10">
-        <input type="text"
-               class="form-control"
-               id="name"
-               name="name"
-               placeholder="@lang('title.name')"
-               value="{{ old('name', $lan->name) }}"
-        >
-    </div>
-</div>
-<div class="row mb-3">
-    <label for="start"
-           class="col-sm-2 col-form-label"
-    >
-        @lang('title.start')
-    </label>
-    <div class="col-sm-4">
-        <input type="text"
-               class="form-control datetimepicker-input"
-               id="start"
-               name="start"
-               placeholder="YYYY-MM-DD HH:MM:SS"
-               value="{{ old('start', $lan->start) }}"
-               data-toggle="datetimepicker"
-               data-target="#start"
-        >
-    </div>
-    <label for="end"
-           class="col-sm-2 col-form-label"
-    >
-        @lang('title.end')
-    </label>
-    <div class="col-sm-4">
-        <input type="text"
-               class="form-control datetimepicker-input"
-               id="end"
-               name="end"
-               placeholder="YYYY-MM-DD HH:MM:SS"
-               value="{{ old('end', $lan->end) }}"
-               data-toggle="datetimepicker"
-               data-target="#end"
-        >
-    </div>
-</div>
-
+@include('components.form.inputs.name', ['value' => $lan->name])
+@include('components.form.inputs.start-end', ['start' => $lan->start, 'end' => $lan->end])
 <div class="row mb-3">
     <label for="venue_id"
            class="col-sm-2 col-form-label"
@@ -86,33 +37,10 @@
         </small>
     </div>
 </div>
-<div class="row mb-3">
-    <label for="published"
-           class="col-sm-2 col-form-label"
-    >
-        @lang('title.published')
-    </label>
-    <div class="col-sm-10">
-        <div class="form-check">
-            <input type="checkbox"
-                   class="form-check-input"
-                   id="published"
-                   name="published"
-                   value="1"
-                {{ old('published', $lan->published) ? 'checked' : null}}
-            >
-        </div>
-    </div>
-</div>
-
-<div class="row mb-3">
-    <div class="offset-sm-2 d-grid col-sm-10 gap-2">
-        <button type="submit" class="btn btn-primary">@lang('title.submit')</button>
-    </div>
-</div>
+@include('components.form.inputs.published', ['value' => $lan->published])
+@include('components.form.inputs.submit')
 <script>
     document.addEventListener("DOMContentLoaded", function () {
-
         const startInput = document.getElementById('start');
         const start = new TempusDominus(startInput, {
             localization: {
