d676f505d6184878017a749baa4ab3c3b5b2c7f1
ilumos <ilumos@gmail.com>
2024-09-20 14:49:06 +0100

    Make guide form horizontal

--- resources/views/pages/guides/partials/form.blade.php
+++ resources/views/pages/guides/partials/form.blade.php
@@ -1,23 +1,50 @@
-<div class="form-group mb-3">
-    <label for="title">@lang('title.title')</label>
-    <input type="text" class="form-control" id="title" name="title" placeholder="@lang('title.title')"
-           value="{{ old('title', $guide->title) }}">
-</div>
-<div class="form-group mb-3">
-    <label for="content">@lang('title.content')</label>
-    <textarea class="form-control" id="content" name="content" rows="10" placeholder="@lang('phrase.markdown-help')"
-              aria-describedby="contentHelp">{{ old('content', $guide->content) }}</textarea>
-    <small id="contentHelp" class="form-text text-muted">
-        <a href="@lang('phrase.markdown-formatting-help-link-url')" target="_blank">@lang('phrase.markdown-formatting-help-link')</a>
-        <br>
-        <a href="{{ route('images.index') }}" target="_blank">@lang('title.upload-images')</a>
-    </small>
-</div>
-<div class="form-group mb-3">
-    <div class="custom-control custom-checkbox">
-        <input type="checkbox" class="custom-control-input" id="published" name="published"
-               value="1" {{ old('published', $guide->published) ? 'checked' : null}}>
-        <label class="custom-control-label" for="published">@lang('title.published')</label>
+<div class="row mb-3">
+    <label for="title"
+           class="col-sm-2 col-form-label"
+    >
+        @lang('title.title')
+    </label>
+    <div class="col-sm-10">
+        <input type="text"
+               class="form-control"
+               id="title"
+               name="title"
+               placeholder="@lang('title.name')"
+               value="{{ old('name', $guide->title) }}"
+        >
     </div>
 </div>
-<button type="submit" class="btn btn-primary">@lang('title.submit')</button>
+
+<div class="row mb-3">
+    <label for="content"
+           class="col-sm-2 col-form-label"
+    >
+        @lang('title.content')
+    </label>
+    <div class="col-sm-10">
+        <textarea class="form-control"
+                  id="content"
+                  name="content"
+                  rows="10"
+                  placeholder="@lang('phrase.markdown-help')"
+                  aria-describedby="contentHelp"
+        >{{ old('content', $guide->content) }}</textarea>
+        <small id="contentHelp"
+               class="form-text text-muted"
+        >
+            <a href="@lang('phrase.markdown-formatting-help-link-url')"
+               target="_blank"
+            >
+                @lang('phrase.markdown-formatting-help-link')
+            </a>
+            <br>
+            <a href="{{ route('images.index') }}"
+               target="_blank"
+            >
+                @lang('title.upload-images')
+            </a>
+        </small>
+    </div>
+</div>
+@include('components.form.inputs.published', ['value' => $guide->published])
+@include('components.form.inputs.submit')
