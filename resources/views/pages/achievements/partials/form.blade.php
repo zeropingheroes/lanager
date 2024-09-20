b956dbca48adf0836d3c2e617b9503ddaa53aa4d
ilumos <ilumos@gmail.com>
2024-09-20 14:11:34 +0100

    Move achievement image below description

--- resources/views/pages/achievements/partials/form.blade.php
+++ resources/views/pages/achievements/partials/form.blade.php
@@ -1,7 +1,7 @@
 {{csrf_field()}}
 
 @include('components.form.inputs.name', ['value' => $achievement->name])
-
+@include('components.form.inputs.description', ['value' => $achievement->description])
 <div class="row mb-3">
     <label for="image"
            class="col-sm-2 col-form-label">
@@ -9,9 +9,7 @@
     </label>
     <div class="col-sm-10">
         <input type="file" class="form-control" id="image" name="image">
-    <small>@lang('phrase.achievement-image-help')</small>
+        <small>@lang('phrase.achievement-image-help')</small>
     </div>
 </div>
-
-@include('components.form.inputs.description', ['value' => $achievement->description])
 @include('components.form.inputs.submit')
