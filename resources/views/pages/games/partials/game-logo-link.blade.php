b4ba7da80fda0de2985bed107c8cf77b0d75b2dd
ilumos <ilumos@gmail.com>
2026-02-11 22:11:51 +0000

    If no logo in database, display default Steam app logo overlaid with app name

--- resources/views/pages/games/partials/game-logo-link.blade.php
+++ resources/views/pages/games/partials/game-logo-link.blade.php
@@ -1,3 +1,9 @@
-<a href="{{ $url }}" title="{{ $name }}">
-    <img src="{{ $logo }}" alt="@lang('phrase.logo-for-game', ['game' => $name])">
-</a>
+    @if(!empty($logo))
+        <a href="{{ $url }}" title="{{ $name }}">
+            <img src="{{ $logo }}" alt="@lang('phrase.logo-for-game', ['game' => $name])">
+        </a>
+    @else
+        <a href="{{ $url }}" class="game-logo-not-found" title="{{ $name }}">
+            {{ Str::limit($name, 20) }}
+        </a>
+    @endif
