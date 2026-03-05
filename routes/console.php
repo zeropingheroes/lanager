e93010c989d2cef3af6f8465ce28c8aa1198cb22
ilumos <ilumos@gmail.com>
2026-03-05 10:40:18 +0000

    Clarify Steam API rate limits that apply to scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -16,21 +16,23 @@ use Zeropingheroes\Lanager\Console\Commands\UpdateSteamUsers;
 |
 */
 
-// Steam limit API calls to 100,000 per day
+// The Steam Web API limits requests to 100,000 per day
 // the below schedules will not exceed this limit for a LAN party of ~1,000 users.
 
-// 1 Steam API call per 100 users
-// e.g. for 1000 users:
-// 10 calls each minute * 1440 minutes in a day = 14,400 daily API calls
+// 1 Steam Web API request per 100 users
+// Example for 1000 users:
+// 10 requests each minute * 1440 minutes in a day = 14,400 daily requests
 Schedule::command(UpdateSteamUsers::class)
     ->everyMinute();
 
-// 1 Steam API call per user
-// e.g. for 1000 users:
-// 1000 calls * 48 half-ours in a day = 48,000 daily API calls
+// 1 Steam Web API request per user
+// Example for 1000 users:
+// 1000 requests * 48 half-ours in a day = 48,000 daily requests
 Schedule::command(UpdateSteamUserApps::class)
     ->everyThirtyMinutes();
 
-// ~3 Steam API calls total
+// 1 Steam Web API request per 50,000 Steam apps
+// Example for ~160,000 steam apps:
+// 4 requests * 24 hours = 96 daily requests
 Schedule::command(UpdateSteamApps::class)
     ->hourly();
