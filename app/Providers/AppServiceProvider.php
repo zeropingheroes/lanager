6bbf49ba5c7ca1797fbb3b3b62134350d2f13c36
ilumos <ilumos@gmail.com>
2026-03-22 18:13:11 +0000

    Move from `barryvdh/laravel-debugbar` to `fruitcake/laravel-debugbar`

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,35 +2,12 @@
 
 namespace Zeropingheroes\Lanager\Providers;
 
-use Barryvdh\Debugbar\Facades\Debugbar;
-use Barryvdh\Debugbar\ServiceProvider as DebugbarServiceProvider;
-use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        // Load debugbar if:
-        // - env set to local
-        // - debug enabled
-        // - class exists
-        if (
-            $this->app->environment('local')
-            && config('app.debug')
-            && class_exists(DebugbarServiceProvider::class)
-        ) {
-            $this->app->register(DebugbarServiceProvider::class);
-            $this->app->alias('Debugbar', Debugbar::class);
-            $this->app->register(IdeHelperServiceProvider::class);
-        }
-    }
-
     /**
      * Bootstrap any application services.
      */
