e3da1f256bb8eab7e75b0a480a9d8808fcf589df
ilumos <ilumos@gmail.com>
2026-04-17 20:46:10 +0100

    Add default avatar for users without an avatar

--- resources/views/pages/users/partials/avatar.blade.php
+++ resources/views/pages/users/partials/avatar.blade.php
@@ -1,8 +1,9 @@
 @if($user->id && $user->username)
     @php
-        $steamAccount = $user->accounts->where('provider','steam')->first();
 
         $size = $size ?? 'small';
+
+        $steamAccount = $user->accounts->where('provider','steam')->first();
 
         if($steamAccount) {
             $avatar = match ($size) {
@@ -11,29 +12,28 @@
                 default => str_replace('_medium.jpg', '.jpg', $steamAccount->avatar),
             };
         } else {
-            $avatar = $user->accounts->first()->avatar;
+            $avatar = Vite::asset('resources/images/default-avatar.png');
         }
 
-        $activeSession = $user->steamAppSessions->first();
+$activeSession = $user->steamAppSessions->first();
 
-        if($activeSession) {
-            $statusName = 'in-game';
-            $statusDisplayName = __('phrase.status-in-game-x', ['x' => $activeSession->app->name]);
-        } elseif(
-            $user->steamMetadata->exists &&
-            $user->steamMetadata->profile_updated_at != null &&
-            $user->steamMetadata->profile_updated_at->greaterThan(now()->subMinutes(3))
-        ) {
-            $statusName = $user->steamMetadata->status->name;
-            $statusDisplayName = $user->steamMetadata->status->display_name;
-        } else {
-            $statusName = 'unknown';
-            $statusDisplayName = __('phrase.status-unknown');
-        }
+if($activeSession) {
+    $statusName = 'in-game';
+    $statusDisplayName = __('phrase.status-in-game-x', ['x' => $activeSession->app->name]);
+} elseif(
+    $user->steamMetadata->exists &&
+    $user->steamMetadata->profile_updated_at != null &&
+    $user->steamMetadata->profile_updated_at->greaterThan(now()->subMinutes(3))
+) {
+    $statusName = $user->steamMetadata->status->name;
+    $statusDisplayName = $user->steamMetadata->status->display_name;
+} else {
+    $statusName = 'unknown';
+    $statusDisplayName = __('phrase.status-unknown');
+}
 
     @endphp
     <img class="avatar avatar-{{ $size }} avatar-{{ $statusName }}"
-         src="{{ $avatar }}"
-         alt="@lang('phrase.avatar-for-username', ['username' => $user->username])"
-         title="{{ $statusDisplayName }}">
-@endif
+     src="{{ $avatar }}"
+     alt="@lang('phrase.avatar-for-username', ['username' => $user->username])"
+     title="{{ $statusDisplayName }}">@endif
