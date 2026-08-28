3599ba6934b040fbf758a7da0caf2fbb3eab6054
ilumos <ilumos@gmail.com>
2026-08-28 23:27:02 +0100

    Replace LAN tab counts with icons

--- resources/views/pages/lans/partials/navigation-tabs.blade.php
+++ resources/views/pages/lans/partials/navigation-tabs.blade.php
@@ -2,14 +2,13 @@
     <li class="nav-item">
         <a class="nav-link {{ Request::route()->getName() == 'lans.lan-games.index' ? 'active' : '' }}"
            href="{{ route('lans.lan-games.index', $lan) }}">
-            @lang('title.games') <span class="badge text-bg-primary">{{ $lan->games->count() }}</span>
+            <i class="fa-solid fa-gamepad"></i> @lang('title.games')
         </a>
     </li>
     <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle {{ Request::route()->getName() == 'lans.events.index' ? 'active' : '' }}"
            data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-            @lang('title.events') <span
-                class="badge text-bg-primary">{{ $lan->events()->where('published', '=', '1')->count() }}</span>
+            <i class="fa-solid fa-calendar"></i> @lang('title.events')
         </a>
         <div class="dropdown-menu">
             <a class="dropdown-item" href="{{ route('lans.events.index', $lan) }}"><i class="fa-solid fa-list"></i> @lang('title.list')</a>
@@ -20,28 +19,26 @@
     <li class="nav-item">
         <a class="nav-link {{ Request::route()->getName() == 'lans.guides.index' ? 'active' : '' }}"
            href="{{ route('lans.guides.index', $lan) }}">
-            @lang('title.guides') <span
-                class="badge text-bg-primary">{{ $lan->guides()->where('published', '=', '1')->count() }}</span>
+            <i class="fa-solid fa-book"></i> @lang('title.guides')
         </a>
     </li>
     <li class="nav-item">
         <a class="nav-link {{ Request::route()->getName() == 'lans.attendees.index' ? 'active' : '' }}"
            href="{{ route('lans.attendees.index', $lan) }}">
-            @lang('title.attendees') <span class="badge text-bg-primary">{{ $lan->users->count() }}</span>
+            <i class="fa-solid fa-users"></i> @lang('title.attendees')
         </a>
     </li>
     <li class="nav-item">
         <a class="nav-link {{ Request::route()->getName() == 'lans.user-achievements.index' ? 'active' : '' }}"
            href="{{ route('lans.user-achievements.index', $lan) }}">
-            @lang('title.achievements') <span class="badge text-bg-primary">{{ $lan->userAchievements->count() }}</span>
+            <i class="fa-solid fa-trophy"></i> @lang('title.achievements')
         </a>
     </li>
     @can('create', \Zeropingheroes\Lanager\Models\Slide::class)
         <li class="nav-item">
             <a class="nav-link {{ Request::route()->getName() == 'lans.slides.index' ? 'active' : '' }}"
                href="{{ route('lans.slides.index', $lan) }}">
-                @lang('title.slides') <span
-                    class="badge text-bg-primary">{{ $lan->slides()->where('published', '=', '1')->count() }}</span>
+                <i class="fa-solid fa-images"></i> @lang('title.slides')
             </a>
         </li>
     @endcan
@@ -49,7 +46,7 @@
         <li class="nav-item">
             <a class="nav-link {{ str_starts_with(Request::route()->getName(), 'lans.discord-channel-webhooks') ? 'active' : '' }}"
                href="{{ route('lans.discord-channel-webhooks.index', $lan) }}">
-                @lang('title.channels') <span class="badge text-bg-primary">{{ $lan->discordChannelWebhooks->count() }}</span>
+                <i class="fa-solid fa-hashtag"></i> @lang('title.channels')
             </a>
         </li>
     @endcan
